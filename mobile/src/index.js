0f62c038a3472e23d63f987801caa32e76b199ef
Douglas Nunes de Mattos <DouglasNunesdeMattos@outlook.com>
2020-07-04 22:52:03 -0300

    Initial commit

---
+++ mobile/src/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+export default () => {
+  return (
+    <View>
+      <Text>Hello React Native</Text>
+      <Text>This will be a amazing application</Text>
+    </View>
+  );
+};
