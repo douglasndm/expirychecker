a880404c71e650f5fa92731de76bcee50cb626b4
Douglas Nunes de Mattos <DouglasNunesdeMattos@outlook.com>
2020-07-08 23:40:55 -0300

    adding the FAB button in home page

--- src/Views/Home/index.js
+++ src/Views/Home/index.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import { View, Text, FlatList } from 'react-native';
+import { useNavigation } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
+import { FAB } from 'react-native-paper';
+import Ionicons from 'react-native-vector-icons/Ionicons';
 import LottieView from 'lottie-react-native';
 
 import CalendarAnimation from '../../Assets/Animations/Calendar_date.json';
@@ -52,17 +55,42 @@ const FooterButton = () => {
 };
 
 const ProductList = () => {
+    const [fabOpen, setFabOpen] = React.useState(false);
+    const navigation = useNavigation();
+
     return (
-        <Container>
-            <FlatList
-                data={products}
-                keyExtractor={(item) => String(item.id)}
-                ListHeaderComponent={ListHeader}
-                renderItem={({ item }) => <ProductItem product={item} />}
-                ListFooterComponent={FooterButton}
-                ListEmptyComponent={EmptyList}
+        <>
+            <Container>
+                <FlatList
+                    data={products}
+                    keyExtractor={(item) => String(item.id)}
+                    ListHeaderComponent={ListHeader}
+                    renderItem={({ item }) => <ProductItem product={item} />}
+                    ListFooterComponent={FooterButton}
+                    ListEmptyComponent={EmptyList}
+                />
+            </Container>
+
+            <FAB.Group
+                actions={[
+                    {
+                        icon: () => (
+                            <Ionicons name="add" size={24} color="#14d48f" />
+                        ),
+                        label: 'Adicionar produto',
+                        onPress: () => {
+                            navigation.navigate('AddProduct');
+                        },
+                    },
+                ]}
+                icon={() => <Ionicons name="reader" size={24} color="#FFF" />}
+                open={fabOpen}
+                onStateChange={() => setFabOpen(!fabOpen)}
+                visible
+                onPress={() => setFabOpen(!fabOpen)}
+                fabStyle={{ backgroundColor: '#14d48f' }}
             />
-        </Container>
+        </>
     );
 };
 
