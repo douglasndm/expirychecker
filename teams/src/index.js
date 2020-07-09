b2c9835ce03c24d7e985ed32bd62c615b50014af
Douglas Nunes de Mattos <DouglasNunesdeMattos@outlook.com>
2020-07-08 23:40:55 -0300

    adding the FAB button in home page

--- teams/src/index.js
+++ teams/src/index.js
@@ -3,17 +3,19 @@ import 'react-native-gesture-handler';
 import React from 'react';
 import { StatusBar } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
-import { Provider as PaperProvider } from 'react-native-paper';
+import { Provider as PaperProvider, Portal } from 'react-native-paper';
 
 import Routes from './routes';
 
 export default () => {
     return (
         <PaperProvider>
-            <NavigationContainer>
-                <StatusBar backgroundColor="#14d48f" />
-                <Routes />
-            </NavigationContainer>
+            <Portal>
+                <NavigationContainer>
+                    <StatusBar backgroundColor="#14d48f" />
+                    <Routes />
+                </NavigationContainer>
+            </Portal>
         </PaperProvider>
     );
 };
