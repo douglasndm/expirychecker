27f51119c87ee735c97ce64537fd0d026f219063
Douglas Nunes de Mattos <DouglasNunesdeMattos@outlook.com>
2020-07-09 16:07:53 -0300

    Setting Realm and starting to showing database's datas on home screen

--- src/Views/Home/index.js
+++ src/Views/Home/index.js
@@ -1,12 +1,12 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { View, Text, FlatList } from 'react-native';
 import { useNavigation } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 import { FAB } from 'react-native-paper';
 import Ionicons from 'react-native-vector-icons/Ionicons';
-import LottieView from 'lottie-react-native';
 
-import CalendarAnimation from '../../Assets/Animations/Calendar_date.json';
+import Realm from '../../Services/Realm';
+import ProductItem from '../../Components/Product';
 
 import {
     Container,
@@ -19,87 +19,128 @@ import {
 } from './styles';
 
 import ProductDetails from '../ProductDetails';
-import ProductItem from '../../Components/Product';
-
-import { products } from '../../products.json';
 
 const StackNavigator = createStackNavigator();
 
-const ListHeader = () => {
-    return (
-        <View>
-            <HeaderContainer>
-                <LottieView source={CalendarAnimation} autoPlay loop />
-                <TextLogo>Controle de validade</TextLogo>
-            </HeaderContainer>
+export default function Home() {
+    const [products, setProducts] = useState([]);
 
-            <CategoryDetails>
-                <CategoryDetailsText>
-                    Produtos mais próximos ao vencimento
-                </CategoryDetailsText>
-            </CategoryDetails>
-        </View>
-    );
-};
+    useEffect(() => {
+        async function getProduts() {
+            const realm = await Realm();
 
-const EmptyList = () => {
-    return <Text>Não há nenhum produto cadastrado ainda...</Text>;
-};
+            try {
+                const results = realm.objects('Product');
 
-const FooterButton = () => {
-    return (
-        <ButtonLoadMore onPress={() => {}}>
-            <ButtonLoadMoreText>Mostrar todos os produtos</ButtonLoadMoreText>
-        </ButtonLoadMore>
-    );
-};
+                setProducts(results);
+            } catch (error) {
+                console.log(error);
+            }
+        }
 
-const ProductList = () => {
-    const [fabOpen, setFabOpen] = React.useState(false);
-    const navigation = useNavigation();
+        getProduts();
+    }, []);
 
-    return (
-        <>
-            <Container>
-                <FlatList
-                    data={products}
-                    keyExtractor={(item) => String(item.id)}
-                    ListHeaderComponent={ListHeader}
-                    renderItem={({ item }) => <ProductItem product={item} />}
-                    ListFooterComponent={FooterButton}
-                    ListEmptyComponent={EmptyList}
-                />
-            </Container>
+    const ListHeader = () => {
+        return (
+            <View>
+                <HeaderContainer>
+                    <TextLogo>Controle de validade</TextLogo>
+                </HeaderContainer>
 
-            <FAB.Group
-                actions={[
-                    {
-                        icon: () => (
-                            <Ionicons name="add" size={24} color="#14d48f" />
-                        ),
-                        label: 'Adicionar produto',
-                        onPress: () => {
-                            navigation.navigate('AddProduct');
+                {/* Verificar se há items antes de criar o titulo */}
+                {products.length > 0 ? (
+                    <CategoryDetails>
+                        <CategoryDetailsText>
+                            Produtos mais próximos ao vencimento
+                        </CategoryDetailsText>
+                    </CategoryDetails>
+                ) : null}
+            </View>
+        );
+    };
+
+    const EmptyList = () => {
+        return (
+            <Text style={{ marginLeft: 15, marginRight: 15 }}>
+                Não há nenhum produto cadastrado ainda...
+            </Text>
+        );
+    };
+
+    function FooterButton() {
+        if (products.length > 5) {
+            return (
+                <ButtonLoadMore onPress={() => {}}>
+                    <ButtonLoadMoreText>
+                        Mostrar todos os produtos
+                    </ButtonLoadMoreText>
+                </ButtonLoadMore>
+            );
+        }
+
+        return (
+            <ButtonLoadMore onPress={() => {}}>
+                <ButtonLoadMoreText>Cadastrar um produto</ButtonLoadMoreText>
+            </ButtonLoadMore>
+        );
+    }
+
+    const ProductList = () => {
+        const [fabOpen, setFabOpen] = React.useState(false);
+        const navigation = useNavigation();
+
+        return (
+            <>
+                <Container>
+                    <FlatList
+                        data={products}
+                        keyExtractor={(item) => String(item.id)}
+                        ListHeaderComponent={ListHeader}
+                        renderItem={({ item }) => (
+                            <ProductItem product={item} />
+                        )}
+                        ListFooterComponent={FooterButton}
+                        ListEmptyComponent={EmptyList}
+                    />
+                </Container>
+
+                <FAB.Group
+                    actions={[
+                        {
+                            icon: () => (
+                                <Ionicons
+                                    name="add"
+                                    size={24}
+                                    color="#14d48f"
+                                />
+                            ),
+                            label: 'Adicionar produto',
+                            onPress: () => {
+                                navigation.navigate('AddProduct');
+                            },
                         },
-                    },
-                ]}
-                icon={() => <Ionicons name="reader" size={24} color="#FFF" />}
-                open={fabOpen}
-                onStateChange={() => setFabOpen(!fabOpen)}
-                visible
-                onPress={() => setFabOpen(!fabOpen)}
-                fabStyle={{ backgroundColor: '#14d48f' }}
-            />
-        </>
-    );
-};
+                    ]}
+                    icon={() => (
+                        <Ionicons name="reader" size={24} color="#FFF" />
+                    )}
+                    open={fabOpen}
+                    onStateChange={() => setFabOpen(!fabOpen)}
+                    visible
+                    onPress={() => setFabOpen(!fabOpen)}
+                    fabStyle={{ backgroundColor: '#14d48f' }}
+                />
+            </>
+        );
+    };
 
-export default () => (
-    <StackNavigator.Navigator headerMode="none">
-        <StackNavigator.Screen name="Default" component={ProductList} />
-        <StackNavigator.Screen
-            name="ProductDetails"
-            component={ProductDetails}
-        />
-    </StackNavigator.Navigator>
-);
+    return (
+        <StackNavigator.Navigator headerMode="none">
+            <StackNavigator.Screen name="Default" component={ProductList} />
+            <StackNavigator.Screen
+                name="ProductDetails"
+                component={ProductDetails}
+            />
+        </StackNavigator.Navigator>
+    );
+}
--- src/Views/TestDB/index.js
+++ src/Views/TestDB/index.js
@@ -1,9 +1,44 @@
-import React from 'react';
-import { View } from 'react-native';
+import React, { useEffect } from 'react';
+import { View, Alert } from 'react-native';
 
-// import { Container } from './styles';
+import Realm from '../../Services/Realm';
 
 const TestDB = () => {
+    useEffect(() => {
+        async function RealmConnection() {
+            const realm = await Realm();
+
+            try {
+                await realm.write(() => {
+                    // BLOCO DE CÓDIGO RESPONSAVEL POR BUSCAR O ULTIMO ID NO BANCO E COLOCAR EM
+                    // UMA VARIAVEL INCREMENTANDO + 1 JÁ QUE O REALM NÃO SUPORTA AUTOINCREMENT (??)
+                    const lastProduct = realm
+                        .objects('Product')
+                        .sorted('id', true)[0];
+
+                    console.log(lastProduct);
+
+                    const nextId = lastProduct == null ? 1 : lastProduct.id + 1;
+
+                    realm.create('Product', {
+                        id: nextId,
+                        name: 'ALGUMA COSISA',
+                    });
+                });
+            } catch (err) {
+                console.log(`erro ai: ${err}`);
+            }
+
+            try {
+                const result = await realm.objects('Product');
+            } catch (err) {
+                Alert.alert(err);
+            }
+        }
+
+        RealmConnection();
+    }, []);
+
     return <View />;
 };
 
