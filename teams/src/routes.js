ef67982407e2e6b08ba28120fcd553f6787b25f9
Douglas Nunes de Mattos <DouglasNunesdeMattos@outlook.com>
2020-07-09 21:18:48 -0300

    refactoring and creating all products page

--- teams/src/routes.js
+++ teams/src/routes.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { createDrawerNavigator } from '@react-navigation/drawer';
 
 import Home from './Views/Home';
+import AllProducts from './Views/AllProducts';
 import AddProduct from './Views/AddProduct';
 
 const Drawer = createDrawerNavigator();
@@ -13,6 +14,11 @@ export default () => {
                 name="Home"
                 component={Home}
                 options={{ title: 'Início' }}
+            />
+            <Drawer.Screen
+                name="AllProducts"
+                component={AllProducts}
+                options={{ title: 'Todos os produtos' }}
             />
             <Drawer.Screen
                 name="AddProduct"
