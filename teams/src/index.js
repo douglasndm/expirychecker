60f04d646d7e178e58211188a82e6a4512aeff7e
Douglas Nunes de Mattos <DouglasNunesdeMattos@outlook.com>
2020-07-13 01:54:48 -0300

    setting appcenter

--- teams/src/index.js
+++ teams/src/index.js
@@ -1,6 +1,8 @@
 import 'react-native-gesture-handler';
 
 import React from 'react';
+import Crashes from 'appcenter-crashes';
+import Analytics from 'appcenter-analytics';
 import { enableScreens } from 'react-native-screens';
 import { StatusBar } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
@@ -9,6 +11,15 @@ import { Provider as PaperProvider, Portal } from 'react-native-paper';
 import Routes from './routes';
 
 enableScreens();
+
+async function disableAppCenterIfInDevMode() {
+    await Crashes.setEnabled(false);
+    await Analytics.setEnabled(false);
+}
+
+if (__DEV__) {
+    disableAppCenterIfInDevMode();
+}
 
 export default () => {
     return (
