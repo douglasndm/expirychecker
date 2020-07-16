4a96cbc61ae83248022acb51017700a5b45ba796
Douglas Nunes de Mattos <DouglasNunesdeMattos@outlook.com>
2020-07-16 20:14:39 -0300

    Adding logo to Drawer menu

--- teams/src/Components/DrawerMenu/styles.js
+++ teams/src/Components/DrawerMenu/styles.js
@@ -2,11 +2,16 @@ import styled from 'styled-components/native';
 
 export const MenuHeader = styled.View`
     background-color: #14d48f;
-    padding: 38px;
+    padding: 25px;
     margin-top: -4px;
 `;
 
 export const MenuHeaderTitle = styled.Text`
     font-size: 36px;
     color: #fff;
+`;
+
+export const LogoImage = styled.Image`
+    width: 90%;
+    height: 65px;
 `;
