0ad51b392de06deba0066fb2403356f9d295a081
Douglas Nunes de Mattos <DouglasNunesdeMattos@outlook.com>
2020-07-18 20:49:02 -0300

    Creating about first style and testing a function to delete all products

--- src/Views/About/styles.js
+++ src/Views/About/styles.js
@@ -1,3 +1,24 @@
 import styled from 'styled-components/native';
 
-export const Container = styled.View``;
+export const Container = styled.View`
+    padding: 16px;
+`;
+
+export const PageTitle = styled.Text`
+    font-size: 28px;
+    font-weight: bold;
+`;
+
+export const AboutSection = styled.View`
+    margin-top: 20px;
+`;
+
+export const ApplicationName = styled.Text`
+    font-size: 20px;
+    font-weight: bold;
+`;
+
+export const ApplicationVersion = styled.Text`
+    font-size: 14px;
+    color: rgba(0, 0, 0, 0.3);
+`;
