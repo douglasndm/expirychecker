59e7e09c6417e461380dcf334e33dea547866652
Douglas Nunes de Mattos <DouglasNunesdeMattos@outlook.com>
2020-07-18 20:49:02 -0300

    Creating about first style and testing a function to delete all products

--- mobile/src/Views/About/index.js
+++ mobile/src/Views/About/index.js
@@ -1,10 +1,37 @@
 import React from 'react';
-import { View } from 'react-native';
+import { View, Text } from 'react-native';
 
-// import { Container } from './styles';
+import {
+    Container,
+    PageTitle,
+    AboutSection,
+    ApplicationName,
+    ApplicationVersion,
+} from './styles';
 
 const About = () => {
-    return <View />;
+    return (
+        <Container>
+            <PageTitle>Sobre</PageTitle>
+
+            <AboutSection>
+                <ApplicationName>Controle de Validade</ApplicationName>
+                <ApplicationVersion>Versão 0.2.0.0</ApplicationVersion>
+            </AboutSection>
+
+            <AboutSection>
+                <Text>Desenvolvido por Douglas Nunes de Mattos</Text>
+            </AboutSection>
+
+            <AboutSection>
+                <Text>Só possível por</Text>
+
+                <View>
+                    <Text>https://www.flaticon.com/authors/srip</Text>
+                </View>
+            </AboutSection>
+        </Container>
+    );
 };
 
 export default About;
