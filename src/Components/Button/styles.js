234646da512dbcc3b0081295bb15c4e1e83c2704
Douglas Nunes de Mattos <DouglasNunesdeMattos@outlook.com>
2020-07-23 20:57:52 -0300

    Creating a generic button to be reused

---
+++ src/Components/Button/styles.js
@@ -0,0 +1,16 @@
+import styled from 'styled-components/native';
+
+export const Button = styled.TouchableOpacity`
+    align-items: center;
+    align-self: center;
+    padding: 22px;
+
+    background-color: #14d48f;
+    border-radius: 12px;
+    margin: 15px 0;
+    elevation: 2;
+`;
+
+export const ButtonText = styled.Text`
+    color: #fff;
+`;
