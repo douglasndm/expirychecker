462931cbf6df0e64d9e4e275d38c553fd221f1fb
Douglas Nunes de Mattos <DouglasNunesdeMattos@outlook.com>
2020-07-28 01:52:20 -0300

    Implementing import/export database function

---
+++ src/Functions/Backup.js
@@ -0,0 +1,104 @@
+import RNFS from 'react-native-fs';
+import Share from 'react-native-share';
+import DocumentPicker from 'react-native-document-picker';
+import CryptoJS from 'crypto-js';
+import EnvConfig from 'react-native-config';
+
+import { createProduct } from './Product';
+import Realm from '../Services/Realm';
+
+export async function ExportBackupFile() {
+    try {
+        const realm = await Realm();
+
+        const allProducts = realm.objects('Product');
+
+        // O REALM GRAVA TUDO EM OBJETOS E NÃO EM ARRAYS
+        // ISSO CONVERTE TUDO EM ARRAYS
+        // APENAS PARA ORGANIZAÇÃO FUTURA
+        const arrayProducts = [];
+
+        allProducts.map((p) => {
+            const arrayLotes = [];
+
+            p.lotes.map((l) => {
+                arrayLotes.push(l);
+            });
+
+            const newProduct = {
+                id: p.id,
+                name: p.name,
+                code: p.code,
+                lotes: arrayLotes,
+            };
+
+            arrayProducts.push(newProduct);
+        });
+
+        // Definindo caminho do arquivo "Controle de validade backup file"
+        // Muito legal o nome da extensão né?
+        const path = `${RNFS.DocumentDirectoryPath}/controledevalidade.cvbf`;
+
+        const encryptedProducts = CryptoJS.AES.encrypt(
+            JSON.stringify(arrayProducts),
+            EnvConfig.APPLICATION_SECRET_BACKUP_CRYPT
+        ).toString();
+
+        // VERIFICA SE O ARQUIVO EXISTE E CASO EXISTA APAGUE ELE
+        // POR ALGUM MOTIVO A LIB FAZ APPEND AUTOMATICO
+        if (await RNFS.exists(path)) {
+            RNFS.unlink(path);
+        }
+
+        // write the file
+        RNFS.writeFile(path, encryptedProducts, 'utf8')
+            .then((success) => {
+                Share.open({
+                    title: 'Salvar arquivo de exportação',
+                    message:
+                        'Escolha um lugar para salvar o arquivo de exportação',
+                    url: `file://${path}`,
+                });
+            })
+            .catch((err) => {
+                console.warn(err.message);
+            });
+    } catch (err) {
+        console.warn(err);
+    }
+}
+
+export async function ImportBackupFile() {
+    try {
+        const filePicked = await DocumentPicker.pick();
+
+        // Separa o nome do arquivo da extensão para fazer a validação da extensão do arquivo
+        const [fileName, extension] = filePicked.name.split('.');
+
+        // caso a extensão do arquivo não for cvbf lança um erro e sai da função
+        if (extension !== 'cvbf') {
+            throw new Error('Extensão invalida');
+        }
+
+        // pega o arquivo temporario gerado pelo filePicker e faz a leitura dele
+        const fileRead = await RNFS.readFile(filePicked.fileCopyUri);
+
+        // decriptografa o arquivo lido
+        const decryptedFile = await CryptoJS.AES.decrypt(
+            fileRead,
+            EnvConfig.APPLICATION_SECRET_BACKUP_CRYPT
+        );
+
+        // converte o arquivo em formato de bytes puros para string
+        const originalFile = decryptedFile.toString(CryptoJS.enc.Utf8);
+
+        // converte tudo de novo para json
+        const products = JSON.parse(originalFile);
+
+        products.map(async (p) => {
+            await createProduct(p);
+        });
+    } catch (err) {
+        console.log(err.message);
+    }
+}
