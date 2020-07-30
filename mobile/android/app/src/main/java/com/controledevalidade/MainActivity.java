a7778ae588a3fce714eaa19fb67ce643b3ecf9f1
Douglas Nunes de Mattos <DouglasNunesdeMattos@outlook.com>
2020-07-30 13:08:10 -0300

    Fixing a crash bug that occured when the app was returning from background

--- mobile/android/app/src/main/java/com/controledevalidade/MainActivity.java
+++ mobile/android/app/src/main/java/com/controledevalidade/MainActivity.java
@@ -1,6 +1,7 @@
 package com.controledevalidade;
 
 import com.facebook.react.ReactActivity;
+import android.os.Bundle;
 
 public class MainActivity extends ReactActivity {
 
@@ -11,5 +12,14 @@ public class MainActivity extends ReactActivity {
   @Override
   protected String getMainComponentName() {
     return "controledevalidade";
+  }
+
+  // RESPONSAVEL POR PARAR OS TRAVAMENTOS EM PRODUÇÃO QUANDO O USUÁRIO TENTA VOLTA PARA O APLICATIVO
+  // DEPOIS DE TER FECHADO ELE
+  // https://github.com/software-mansion/react-native-screens/issues/114
+  @Override
+  protected void onCreate(Bundle savedInstance){
+        // super.onCreate(savedInstance);
+        super.onCreate(null);
   }
 }
