25b38fbafa6e1f1580f41c1c8d19743c0cebab58
Douglas Nunes de Mattos <DouglasNunesdeMattos@outlook.com>
2020-08-02 15:22:21 -0300

    Changing the ads config file to a separate file and adding a hidden function to hide ads

---
+++ src/Services/Admob.js
@@ -0,0 +1,26 @@
+import admob, { MaxAdContentRating } from '@react-native-firebase/admob';
+import { getAdsEnabled, setAdsEnabled } from '../Functions/Settings';
+
+async function prepareAds() {
+    await setAdsEnabled(true); // <- Will be removed in the future
+
+    if (await getAdsEnabled) {
+        admob()
+            .setRequestConfiguration({
+                // Update all future requests suitable for parental guidance
+                maxAdContentRating: MaxAdContentRating.PG,
+
+                // Indicates that you want your content treated as child-directed for purposes of COPPA.
+                tagForChildDirectedTreatment: false,
+
+                // Indicates that you want the ad request to be handled in a
+                // manner suitable for users under the age of consent.
+                tagForUnderAgeOfConsent: true,
+            })
+            .catch((err) => {
+                console.warn(err);
+            });
+    }
+}
+
+prepareAds();
