e9806bf11a7d77cf6b5daa4d34e7aa6966771c9d
Douglas Nunes de Mattos <DouglasNunesdeMattos@outlook.com>
2020-08-23 20:53:00 -0300

    Updating Menu and Generic button to typescript

---
+++ src/Components/DrawerMenu/styles.ts
@@ -0,0 +1,30 @@
+import styled from 'styled-components/native';
+import { DrawerItem } from '@react-navigation/drawer';
+import Ionicons from 'react-native-vector-icons/Ionicons';
+
+export const Container = styled.View`
+    flex: 1;
+    background-color: ${({ theme }) => theme.colors.background};
+`;
+
+export const MenuHeader = styled.View`
+    background-color: ${({ theme }) => theme.colors.accent};
+    padding: 25px;
+    margin-top: -5px;
+`;
+
+export const MenuHeaderTitle = styled.Text`
+    font-size: 36px;
+    color: #fff;
+`;
+
+export const LogoImage = styled.Image`
+    width: 90%;
+    height: 65px;
+`;
+
+export const MenuItem = styled(DrawerItem)``;
+
+export const Icons = styled(Ionicons)`
+    color: ${({ theme }) => theme.colors.text};
+`;
