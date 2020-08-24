ca35b758fe3e51976b031d98646ea81f39352156
Douglas Nunes de Mattos <DouglasNunesdeMattos@outlook.com>
2020-08-24 18:50:51 -0300

    About page converted to use theme in styled components

---
+++ mobile/src/Views/About/styles.ts
@@ -0,0 +1,42 @@
+import styled from 'styled-components/native';
+import Ionicons from 'react-native-vector-icons/Ionicons';
+
+export const Container = styled.View`
+    flex: 1;
+    padding: 16px 10px;
+    background: ${(props) => props.theme.colors.background};
+`;
+
+export const Content = styled.View`
+    flex-direction: row;
+    margin-left: -15px;
+`;
+
+export const PageTitle = styled.Text`
+    font-size: 28px;
+    font-weight: bold;
+    color: ${(props) => props.theme.colors.text};
+`;
+
+export const AboutSection = styled.View`
+    margin-top: 20px;
+`;
+
+export const ApplicationName = styled.Text`
+    font-size: 20px;
+    font-weight: bold;
+    color: ${(props) => props.theme.colors.text};
+`;
+
+export const ApplicationVersion = styled.Text`
+    font-size: 14px;
+    color: ${(props) => props.theme.colors.subText};
+`;
+
+export const Icons = styled(Ionicons)`
+    color: ${(props) => props.theme.colors.text};
+`;
+
+export const Text = styled.Text`
+    color: ${(props) => props.theme.colors.text};
+`;
