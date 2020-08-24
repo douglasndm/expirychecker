26ab3babe1cbb5c853a4f73414c1e4e2284e60e9
Douglas Nunes de Mattos <DouglasNunesdeMattos@outlook.com>
2020-08-24 17:33:50 -0300

    Updating Realm

--- src/Services/Realm.ts
+++ src/Services/Realm.ts
@@ -4,7 +4,7 @@ import { ProductSchema } from '../Schemas/ProductSchema';
 import { LoteSchema } from '../Schemas/LoteSchema';
 import SettingSchema from '../Schemas/SettingSchema';
 
-export default () => {
+export default (): ProgressPromise => {
     return Realm.open({
         schema: [ProductSchema, LoteSchema, SettingSchema],
         schemaVersion: 1,
