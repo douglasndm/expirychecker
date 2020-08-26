67caa831acfdcc2b9c15b562a1ba7ce2fa03c79d
Douglas Nunes de Mattos <DouglasNunesdeMattos@outlook.com>
2020-08-25 21:20:10 -0300

    Finishing changing all pages to typerscript

---
+++ src/Views/ProductDetails/styles.ts
@@ -0,0 +1,116 @@
+import styled, { css } from 'styled-components/native';
+import { Button, DataTable, Dialog } from 'react-native-paper';
+import Ionicons from 'react-native-vector-icons/Ionicons';
+
+interface IProduct {
+    expired?: boolean;
+    nextToExp?: boolean;
+    expiredOrNext?: boolean;
+}
+
+export const Container = styled.View`
+    flex: 1;
+    padding: 16px;
+    elevation: 2;
+
+    background: ${({ theme }) => theme.colors.background};
+`;
+
+export const PageHeader = styled.View`
+    flex-direction: row;
+    justify-content: space-between;
+    align-items: center;
+`;
+
+export const ProductDetailsContainer = styled.View`
+    max-width: 60%;
+`;
+
+export const PageTitle = styled.Text`
+    font-size: 28px;
+    font-weight: bold;
+
+    color: ${({ theme }) => theme.colors.text};
+`;
+
+export const ProductName = styled.Text`
+    font-size: 24px;
+    margin-left: 5px;
+
+    color: ${({ theme }) => theme.colors.text};
+`;
+
+export const ProductCode = styled.Text`
+    font-size: 18px;
+    margin-left: 5px;
+
+    margin-bottom: 7px;
+
+    color: ${({ theme }) => theme.colors.text};
+`;
+
+export const ButtonPaper = styled(Button).attrs((props) => ({
+    color: props.theme.colors.accent,
+}))`
+    color: ${({ theme }) => theme.colors.text};
+`;
+
+export const Icons = styled(Ionicons)`
+    color: ${({ theme }) => theme.colors.text};
+`;
+
+export const CategoryDetails = styled.View`
+    flex-direction: row;
+    justify-content: space-between;
+
+    margin: 10px 5px;
+`;
+
+export const CategoryDetailsText = styled.Text`
+    color: ${(props) => props.theme.colors.accent};
+    font-size: 18px;
+`;
+
+export const Table = styled(DataTable)`
+    background-color: ${(props) => props.theme.colors.productBackground};
+    border-radius: 12px;
+`;
+
+export const TableHeader = styled(DataTable.Header)``;
+
+export const TableTitle = styled(DataTable.Title)`
+    color: rgba(255, 255, 255, 1);
+`;
+
+export const TableRow = styled(DataTable.Row)<IProduct>`
+    background: ${({ theme }) => theme.colors.productBackground};
+
+    ${(props) =>
+        props.nextToExp &&
+        css`
+            background: ${({ theme }) =>
+                theme.colors.productNextToExpBackground};
+        `}
+
+    ${(props) =>
+        props.expired &&
+        css`
+            background: ${({ theme }) => theme.colors.productExpiredBackground};
+        `}
+`;
+
+export const TableCell = styled(DataTable.Cell)``;
+
+export const Text = styled.Text<IProduct>`
+    color: ${(props) => props.theme.colors.text};
+
+    ${(props) =>
+        props.expiredOrNext &&
+        css`
+            color: white;
+        `}
+`;
+
+export const DialogPaper = styled(Dialog)`
+    background: ${(props) => props.theme.colors.productBackground};
+`;
