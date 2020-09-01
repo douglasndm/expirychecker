beb258aa0d29308b254d472a1fa5e5f3c356844d
Douglas Nunes de Mattos <DouglasNunesdeMattos@outlook.com>
2020-08-31 21:57:37 -0300

    Moving product container to a separate component

---
+++ src/Components/ProductItem/index.tsx
@@ -0,0 +1,58 @@
+import React, { useState } from 'react';
+import { BannerAd, BannerAdSize } from '@react-native-firebase/admob';
+import crashlytics from '@react-native-firebase/crashlytics';
+import { addDays, isPast } from 'date-fns';
+
+import ProductCard from '../ProductCard';
+
+import { AdView } from './styles';
+
+interface RequestProps {
+    product: IProduct;
+    index: number;
+    adUnitId: string;
+    daysToBeNext: number;
+    isPremium: boolean;
+}
+
+const ProductItem: React.FC<RequestProps> = ({
+    product,
+    index,
+    adUnitId,
+    daysToBeNext,
+    isPremium,
+}: RequestProps) => {
+    const [adFailed, setAdFailed] = useState(false);
+
+    const expired = product.lotes[0] && isPast(product.lotes[0].exp_date);
+    const nextToExp =
+        product.lotes[0] &&
+        addDays(new Date(), daysToBeNext) >= product.lotes[0].exp_date;
+
+    return (
+        <>
+            {!isPremium && index !== 0 && index % 5 === 0 && !adFailed && (
+                <AdView>
+                    <BannerAd
+                        unitId={adUnitId}
+                        size={BannerAdSize.BANNER}
+                        onAdFailedToLoad={(err: Error) => {
+                            setAdFailed(true);
+                            crashlytics().log(
+                                `Falha ao carregar o anúncio ${err}`
+                            );
+                        }}
+                    />
+                </AdView>
+            )}
+
+            <ProductCard
+                product={product}
+                expired={expired}
+                nextToExp={nextToExp}
+            />
+        </>
+    );
+};
+
+export default ProductItem;
---
+++ src/Components/ProductItem/styles.ts
@@ -0,0 +1,8 @@
+import styled from 'styled-components/native';
+
+export const AdView = styled.View`
+    flex: 1;
+    align-items: center;
+    margin-top: 5px;
+    margin-bottom: 5px;
+`;
