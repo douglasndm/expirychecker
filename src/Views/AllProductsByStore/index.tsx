fa85678e9f4fbf180882375ea8086fd9e3a7c2e5
Douglas Nunes de Mattos <DouglasNunesdeMattos@outlook.com>
2020-10-14 01:05:04 -0300

    Now store groups sorts products by exp date

--- src/Views/AllProductsByStore/index.tsx
+++ src/Views/AllProductsByStore/index.tsx
@@ -1,23 +1,17 @@
 import React, { useState, useEffect } from 'react';
 import { ScrollView } from 'react-native';
-import { BannerAd, BannerAdSize, TestIds } from '@react-native-firebase/admob';
-import crashlytics from '@react-native-firebase/crashlytics';
 
-import { getDaysToBeNextToExp } from '../../Functions/Settings';
 import { GetAllProductsOrderedByStore } from '../../Functions/StoresGroup';
 
-import ProductItem from '../../Components/ProductItem';
+import StoreGroup from '../../Components/StoreGroup';
 import Header from '../../Components/Header';
 
-import { AdView } from '../../Components/ProductItem/styles';
-import { Container, StoreGroupContainer, StoreTitle } from './styles';
+import { Container } from './styles';
 
 const AllProductsByStore: React.FC = () => {
     const [allProducts, setAllProducsts] = useState<IStoreGroup[]>([]);
-    const [daysToBeNext, setDaysToBeNext] = useState<number>(30);
 
     useEffect(() => {
-        getDaysToBeNextToExp().then((days) => setDaysToBeNext(days));
         GetAllProductsOrderedByStore().then((data) => setAllProducsts(data));
     }, []);
 
@@ -25,28 +19,12 @@ const AllProductsByStore: React.FC = () => {
         <Container>
             <ScrollView>
                 <Header title="Todos os produtos por loja" />
-                {allProducts.map((storeGroup) => {
-                    return (
-                        <StoreGroupContainer>
-                            <StoreTitle>Loja {storeGroup.name}</StoreTitle>
-
-                            {storeGroup.products.map((product) => (
-                                <ProductItem
-                                    product={product}
-                                    daysToBeNext={daysToBeNext}
-                                    disableAds
-                                />
-                            ))}
-
-                            <AdView>
-                                <BannerAd
-                                    unitId={TestIds.BANNER}
-                                    size={BannerAdSize.BANNER}
-                                />
-                            </AdView>
-                        </StoreGroupContainer>
-                    );
-                })}
+                {allProducts.map((storeGroup) => (
+                    <StoreGroup
+                        storeName={storeGroup.name}
+                        products={storeGroup.products}
+                    />
+                ))}
             </ScrollView>
         </Container>
     );
