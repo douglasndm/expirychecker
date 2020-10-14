c2becb48978a7c435d7d867fe179d4b87d65b63a
Douglas Nunes de Mattos <DouglasNunesdeMattos@outlook.com>
2020-10-14 01:05:04 -0300

    Now store groups sorts products by exp date

--- mobile/src/Views/AllProductsByStore/styles.ts
+++ mobile/src/Views/AllProductsByStore/styles.ts
@@ -4,16 +4,3 @@ export const Container = styled.View`
     flex: 1;
     background-color: ${({ theme }) => theme.colors.background};
 `;
-
-export const StoreGroupContainer = styled.View`
-    margin-bottom: 10px;
-`;
-
-export const StoreTitle = styled.Text`
-    color: ${({ theme }) => theme.colors.text};
-    font-size: 22px;
-    font-weight: bold;
-    margin-left: 15px;
-    margin-right: 15px;
-    margin-top: 10px;
-`;
