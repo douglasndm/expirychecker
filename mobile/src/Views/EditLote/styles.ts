c52c8ab9b3cb7b504df071eb5019ae7a4c565f94
Douglas Nunes de Mattos <DouglasNunesdeMattos@outlook.com>
2020-10-18 00:05:36 -0300

    Adding a function to set product price when adding it on first time

--- mobile/src/Views/EditLote/styles.ts
+++ mobile/src/Views/EditLote/styles.ts
@@ -1,7 +1,6 @@
 import styled from 'styled-components/native';
 import { Button as ButtonPaper } from 'react-native-paper';
 import Ionicons from 'react-native-vector-icons/Ionicons';
-import NumericInput from '@wwdrew/react-native-numeric-textinput';
 
 export const Button = styled(ButtonPaper).attrs((props) => ({
     color: props.theme.colors.accent,
@@ -9,16 +8,4 @@ export const Button = styled(ButtonPaper).attrs((props) => ({
 
 export const Icons = styled(Ionicons)`
     color: ${({ theme }) => theme.colors.text};
-`;
-
-export const NumericInputField = styled(NumericInput).attrs((props) => ({
-    placeholderTextColor: props.theme.colors.subText,
-}))`
-    border: 1px solid rgba(0, 0, 0, 0.1);
-    margin-bottom: 10px;
-    border-radius: 12px;
-    padding: 15px;
-    font-size: 18px;
-    background-color: ${({ theme }) => theme.colors.inputBackground};
-    color: ${({ theme }) => theme.colors.inputText};
 `;
