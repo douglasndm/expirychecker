cb91222e8e7201dc5792e088cb3d5aad32292d2c
Douglas Nunes de Mattos <DouglasNunesdeMattos@outlook.com>
2020-10-20 00:54:01 -0300

    Fix: types for styled components

--- mobile/src/@types/theme.d.ts
+++ mobile/src/@types/theme.d.ts
@@ -1,3 +1,30 @@
+import 'styled-components';
+
+declare module 'styled-components' {
+    export interface DefaultTheme {
+        dark: boolean;
+        mode: 'exact' | 'adaptive';
+
+        colors: {
+            primary: string;
+            accent: string;
+            background: string;
+            text: string;
+            subText: string;
+            textAccent: string;
+
+            inputText: string;
+            inputBackground: string;
+
+            productBackground: string;
+
+            productExpiredBackground: string;
+            productNextToExpBackground: string;
+            productNextOrExpiredText: string;
+        };
+    }
+}
+
 interface ITheme {
     dark: boolean;
     mode: 'exact' | 'adaptive';
