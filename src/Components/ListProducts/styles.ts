255f841444dd28ef44e6fa6b2ffd65c1a58e78c1
Douglas Nunes de Mattos <DouglasNunesdeMattos@outlook.com>
2020-10-22 18:33:27 -0300

    fix: all products without header and list of products with background too dark

--- src/Components/ListProducts/styles.ts
+++ src/Components/ListProducts/styles.ts
@@ -2,7 +2,6 @@ import styled from 'styled-components/native';
 
 export const Container = styled.View`
     flex: 1;
-    background: ${(props) => props.theme.colors.background};
 `;
 
 export const CategoryDetails = styled.View`
