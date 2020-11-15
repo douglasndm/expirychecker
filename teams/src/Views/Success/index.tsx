f680b756b42f77908ef1db6a8433b56ccadedd2e
Douglas Nunes de Mattos <DouglasNunesdeMattos@outlook.com>
2020-11-15 03:48:40 -0300

    Adding more options to success page

--- teams/src/Views/Success/index.tsx
+++ teams/src/Views/Success/index.tsx
@@ -1,5 +1,5 @@
 import React, { useCallback, useMemo } from 'react';
-import { useNavigation } from '@react-navigation/native';
+import { useNavigation, useRoute } from '@react-navigation/native';
 import LottieView from 'lottie-react-native';
 import { BannerAd, BannerAdSize, TestIds } from '@react-native-firebase/admob';
 import EnvConfig from 'react-native-config';
@@ -14,11 +14,31 @@ import {
 } from './styles';
 
 interface Props {
-    type: 'batch' | 'product';
+    type: 'create_batch' | 'create_product' | 'delete_batch' | 'delete_product';
 }
 
-const Success: React.FC<Props> = ({ type }: Props) => {
+const Success: React.FC = () => {
+    const route = useRoute();
     const { reset } = useNavigation();
+
+    const routeParams = route.params as Props;
+
+    const type = useMemo(() => {
+        return routeParams.type;
+    }, [routeParams]);
+
+    const animation = useMemo(() => {
+        if (type === 'create_batch')
+            return require('../../Assets/Animations/success-loading.json');
+        if (type === 'create_product')
+            return require('../../Assets/Animations/success-loading.json');
+        if (type === 'delete_batch')
+            return require('../../Assets/Animations/delete-animation.json');
+        if (type === 'delete_product')
+            return require('../../Assets/Animations/delete-animation.json');
+
+        return require('../../Assets/Animations/success-loading.json');
+    }, [type]);
 
     const handleNavigateToHome = useCallback(() => {
         reset({
@@ -28,26 +48,56 @@ const Success: React.FC<Props> = ({ type }: Props) => {
     }, [reset]);
 
     const adUnitId = useMemo(() => {
-        return __DEV__
-            ? TestIds.BANNER
-            : EnvConfig.ANDROID_ADMOB_ADUNITID_SUCCESSSCREENAFTERADDAPRODUCT;
-    }, []);
+        if (__DEV__) return TestIds.BANNER;
+
+        if (type === 'create_batch')
+            return EnvConfig.ANDROID_ADMOB_ADUNITID_SUCCESSSCREENAFTERADDABATCH;
+
+        if (type === 'create_product')
+            return EnvConfig.ANDROID_ADMOB_ADUNITID_SUCCESSSCREENAFTERADDAPRODUCT;
+
+        if (type === 'delete_batch')
+            return EnvConfig.ANDROID_ADMOB_ADUNITID_SUCCESSSCREENAFTERDELETEABATCH;
+
+        if (type === 'delete_product')
+            return EnvConfig.ANDROID_ADMOB_ADUNITID_SUCCESSSCREENAFTERDELETEAPRODUCT;
+
+        return EnvConfig.ANDROID_ADMOB_ADUNITID_SUCCESSSCREENAFTERADDAPRODUCT;
+    }, [type]);
 
     return (
         <Container>
             <SuccessMessageContainer>
                 <LottieView
-                    source={require('../../Assets/Animations/success-loading.json')}
+                    source={animation}
                     autoPlay
                     loop={false}
                     style={{ width: 180, height: 180 }}
                 />
 
-                <Title>Produto cadastrado</Title>
+                {type === 'create_batch' && <Title>Lote cadastrado</Title>}
+                {type === 'create_product' && <Title>Produto cadastrado</Title>}
+                {type === 'delete_batch' && <Title>Lote apagado</Title>}
+                {type === 'delete_product' && <Title>Produto apagado</Title>}
 
-                <Description>
-                    Seu produto foi cadastrado com sucesso.
-                </Description>
+                {type === 'create_batch' && (
+                    <Description>
+                        Seu lote foi cadastrado com sucesso.
+                    </Description>
+                )}
+                {type === 'create_product' && (
+                    <Description>
+                        Seu produto foi cadastrado com sucesso.
+                    </Description>
+                )}
+                {type === 'delete_batch' && (
+                    <Description>Seu lote foi apagado com sucesso.</Description>
+                )}
+                {type === 'delete_product' && (
+                    <Description>
+                        Seu produto foi apagado com sucesso.
+                    </Description>
+                )}
 
                 <Button
                     text="Voltar ao início"
