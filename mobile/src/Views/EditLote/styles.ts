6cccb01794eb6b55c89400ec5ce64d650ebeeb45
Douglas Nunes de Mattos <DouglasNunesdeMattos@outlook.com>
2020-11-15 03:50:10 -0300

    Moving user to success page after delete a batch and sending the product id instant product object to editLote page

--- mobile/src/Views/EditLote/styles.ts
+++ mobile/src/Views/EditLote/styles.ts
@@ -2,6 +2,13 @@ import styled from 'styled-components/native';
 import { Button as ButtonPaper } from 'react-native-paper';
 import Ionicons from 'react-native-vector-icons/Ionicons';
 
+export const LoadingText = styled.Text``;
+
+export const PageTitleContainer = styled.View`
+    flex-direction: row;
+    align-items: center;
+`;
+
 export const Button = styled(ButtonPaper).attrs((props) => ({
     color: props.theme.colors.accent,
 }))``;
