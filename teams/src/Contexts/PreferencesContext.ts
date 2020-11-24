030bd4a92a9e984f62999f1038415382ca338a11
Douglas Nunes de Mattos <DouglasNunesdeMattos@outlook.com>
2020-11-23 21:08:11 -0300

    Moving all change theme logit to preferences context

--- teams/src/Contexts/PreferencesContext.ts
+++ teams/src/Contexts/PreferencesContext.ts
@@ -1,10 +1,27 @@
 import { createContext } from 'react';
+import { DefaultTheme } from 'styled-components';
+import Themes from '../Themes';
+
+interface IUserPreferences {
+    howManyDaysToBeNextToExpire: number;
+    isUserPremium: boolean;
+    appTheme: DefaultTheme;
+    multiplesStores: boolean;
+}
 
 const PreferencesContext = createContext({
-    howManyDaysToBeNextToExpire: 30,
-    isUserPremium: false,
-    appTheme: 'system',
-    multiplesStores: false,
+    userPreferences: {
+        howManyDaysToBeNextToExpire: 30,
+        isUserPremium: false,
+        appTheme: Themes.Light,
+        multiplesStores: false,
+    },
+    setUserPreferences: ({
+        howManyDaysToBeNextToExpire,
+        isUserPremium,
+        appTheme,
+        multiplesStores,
+    }: IUserPreferences) => {},
 });
 
 export default PreferencesContext;
