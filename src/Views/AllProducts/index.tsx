8c4f7b7aaac818bb365484c2a6b40834d8fc3f10
Douglas Nunes de Mattos <DouglasNunesdeMattos@outlook.com>
2020-12-01 00:25:28 -0300

    All products is now using typeorm

--- src/Views/AllProducts/index.tsx
+++ src/Views/AllProducts/index.tsx
@@ -1,15 +1,15 @@
-import React, { useState, useEffect, useCallback, useContext } from 'react';
+import React, { useState, useEffect, useCallback } from 'react';
+
+import { Product } from '../../Models/Product';
 
 import Header from '../../Components/Header';
 import ListProducts from '../../Components/ListProducts';
 import FABProducts from '../../Components/FABProducts';
 
-import RealmContext from '../../Contexts/RealmContext';
-
 import {
-    sortProductsLotesByLotesExpDate,
-    sortProductsByFisrtLoteExpDate,
-    GetAllProducts,
+    getAllProducts,
+    sortProductsBatchesByBatchExpDate,
+    sortProductsByFirstBatchExpDate,
 } from '../../Functions/Products';
 
 import { InputSearch } from '../Home/styles';
@@ -17,8 +17,6 @@ import { InputSearch } from '../Home/styles';
 import { Container } from './styles';
 
 const AllProducts: React.FC = () => {
-    const { Realm } = useContext(RealmContext);
-
     const [products, setProducts] = useState<Array<IProduct>>([]);
 
     const [searchString, setSearchString] = useState<string>();
@@ -26,7 +24,7 @@ const AllProducts: React.FC = () => {
 
     const getProducts = useCallback(async () => {
         try {
-            const allProducts: Array<IProduct> = await GetAllProducts();
+            const allProducts: Array<Product> = await getAllProducts();
 
             // APARENTEMENTE O REALM SO CONSULTA O PRIMEIRO REGISTRO DE ARRAY PARA FAZER O 'WHERE'
             // ESSA FUNÇÃO REMOVE QUALQUER VESTIGIO DE LOTES TRATADOS
@@ -35,12 +33,12 @@ const AllProducts: React.FC = () => {
             // PRIMEIRO PRECISEI PERCORRER TODOS OS RESULTADOS E ORDERNAR CADA LOTE INDIVIDUALMENTE
             // E COM ISSO RETORNA UM NOVO ARRAY DE OBJETO, PQ NAO ERA POSSIVEL RETORNA O
             // ANTIGO MODIFICADO
-            const resultsTemp = sortProductsLotesByLotesExpDate(allProducts);
+            const resultsTemp = sortProductsBatchesByBatchExpDate(allProducts);
             // classifica os produtos em geral pelo o mais proximo de vencer
             // DEPOIS DE TER TODOS OS PRODUTOS COM OS SEUS LOTES ORDENADOS POR VENCIMENTO, SIMPLISMENTE PEGO O
             // PRIMEIRO LOTE DE CADA PRODUTO(JÁ QUE SEMPRE SERÁ O MAIS PROXIMO A VENCER) E FAÇO A ORDENAÇÃO
             // DE TODOS OS PRODUTOS BASEADO NESTE PRIMEIRO LOTE
-            const results = sortProductsByFisrtLoteExpDate(resultsTemp);
+            const results = sortProductsByFirstBatchExpDate(resultsTemp);
 
             setProducts(results);
         } catch (error) {
@@ -49,18 +47,8 @@ const AllProducts: React.FC = () => {
     }, []);
 
     useEffect(() => {
-        async function startRealm() {
-            Realm.addListener('change', () => getProducts());
-
-            getProducts();
-        }
-
-        startRealm();
-
-        return () => {
-            Realm.removeAllListeners();
-        };
-    }, []);
+        getProducts();
+    }, [getProducts]);
 
     useEffect(() => {
         setProductsSearch(products);
