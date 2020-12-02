aeb73f93ffa00b5fd3dfbe857165e86f39e76eb0
Douglas Nunes de Mattos <DouglasNunesdeMattos@outlook.com>
2020-12-02 00:08:52 -0300

    Creating style for Migrate page

--- src/Views/Migration/index.tsx
+++ src/Views/Migration/index.tsx
@@ -1,18 +1,45 @@
-import React, { useCallback, useEffect } from 'react';
-import { migrateSettings } from '../../Functions/Migration';
+import React, { useCallback, useEffect, useState } from 'react';
+import GenericButton from '../../Components/Button';
+import { migrateProducts, migrateSettings } from '../../Functions/Migration';
 
-import { Container } from './styles';
+import { Container, Loading, MigrateText } from './styles';
 
 const Migration: React.FC = () => {
+    const [isMigrating, setIsMigrating] = useState<boolean>(true);
+
     const migrateData = useCallback(async () => {
+        setIsMigrating(true);
+
         await migrateSettings();
+        await migrateProducts();
+
+        setIsMigrating(false);
     }, []);
 
     useEffect(() => {
         migrateData();
     }, [migrateData]);
 
-    return <Container />;
+    return (
+        <Container>
+            {isMigrating && (
+                <>
+                    <Loading />
+                    <MigrateText>
+                        Aguarde enquanto preparamos o aplicativo
+                    </MigrateText>
+                </>
+            )}
+
+            {!isMigrating && (
+                <>
+                    <MigrateText>Tudo pronto!</MigrateText>
+
+                    <GenericButton text="Vamos começar" onPress={() => {}} />
+                </>
+            )}
+        </Container>
+    );
 };
 
 export default Migration;
