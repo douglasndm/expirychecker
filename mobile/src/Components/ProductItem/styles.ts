f8f66581dac9b8f9349bc55e66b763865fc12055
Douglas Nunes de Mattos <DouglasNunesdeMattos@outlook.com>
2020-12-09 21:06:21 -0300

    Improving Product card speed and fixing ad view bug (ads were hidden)

--- mobile/src/Components/ProductItem/styles.ts
+++ mobile/src/Components/ProductItem/styles.ts
@@ -1,7 +1,8 @@
 import styled from 'styled-components/native';
 
+export const Container = styled.View``;
+
 export const AdView = styled.View`
-    flex: 1;
     align-items: center;
     margin-top: 5px;
     margin-bottom: 5px;
