2ceebdc5b4c28589e2ce3507e6fadde40b560067
Douglas Nunes de Mattos <DouglasNunesdeMattos@outlook.com>
2020-12-13 18:23:06 -0300

    Adding a firebase autheticate with Google to set user id in app to send to revenuecat and restore old pro accounts

---
+++ mobile/src/Views/Auth/SignIn/index.tsx
@@ -0,0 +1,76 @@
+import React, { useCallback, useState } from 'react';
+import { useNavigation } from '@react-navigation/native';
+
+import { setUserId } from '../../../Functions/User';
+import { signInWithGoogle } from '../../../Functions/Auth/Google';
+
+import Header from '../../../Components/Header';
+import GenericButton from '../../../Components/Button';
+
+import {
+    Container,
+    TextsContainer,
+    FirstText,
+    SecondText,
+    ThirdText,
+    LoginText,
+    GoogleButton,
+} from './styles';
+
+const SignIn: React.FC = () => {
+    const [completed, setCompleted] = useState<boolean>(false);
+
+    const { navigate } = useNavigation();
+
+    const handleGoogleButtonPressed = useCallback(async () => {
+        try {
+            setCompleted(false);
+
+            const user = await signInWithGoogle();
+
+            await setUserId(user.uid);
+
+            setCompleted(true);
+        } catch (err) {
+            console.warn(err);
+        }
+    }, []);
+
+    const handleToGoProPage = useCallback(() => {
+        navigate('PremiumSubscription');
+    }, [navigate]);
+    return (
+        <Container>
+            <Header title="Identifique-se" />
+            <TextsContainer>
+                <FirstText>
+                    A autenticação com uma conta do Google é necessária para
+                    continuar.
+                </FirstText>
+
+                <SecondText>
+                    O aplicativo precisa de uma forma de identificar você caso
+                    troque de telefone ou restaure este, por isso pedimos que
+                    faça login com sua conta do Google.
+                </SecondText>
+
+                <ThirdText>
+                    Desta forma podemos restaurar sua conta PRO sem a
+                    necessidade de realizar uma nova assinatura
+                </ThirdText>
+            </TextsContainer>
+
+            <LoginText>{completed ? 'Tudo certo' : 'Fazer login'}</LoginText>
+            {!completed && <GoogleButton onPress={handleGoogleButtonPressed} />}
+
+            {completed && (
+                <GenericButton
+                    text="Continuar para a assinatura"
+                    onPress={handleToGoProPage}
+                />
+            )}
+        </Container>
+    );
+};
+
+export default SignIn;
