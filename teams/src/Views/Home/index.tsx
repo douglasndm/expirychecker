52724e2d58ad1828f00438a674a99cc49cd916d3
Douglas Nunes de Mattos <DouglasNunesdeMattos@outlook.com>
2020-12-13 19:46:52 -0300

    Home now uses new function to get all products

--- teams/src/Views/Home/index.tsx
+++ teams/src/Views/Home/index.tsx
@@ -1,12 +1,10 @@
 import React, { useState, useEffect, useCallback } from 'react';
 
 import {
-    GetAllProductsWithLotesAndNotTratado,
     removeAllLotesTratadosFromAllProduts,
-    sortProductsLotesByLotesExpDate,
-    sortProductsByFisrtLoteExpDate,
     searchForAProductInAList,
     GetAllProducts,
+    getAllProducts,
 } from '../../Functions/Products';
 
 import Loading from '../../Components/Loading';
@@ -39,35 +37,17 @@ const Home: React.FC = () => {
     async function getProduts() {
         try {
             setIsLoading(true);
-            const resultsDB = await GetAllProductsWithLotesAndNotTratado();
+            const allProducts = await getAllProducts({
+                limit: 20,
+                removeProductsWithoutBatches: true,
+                sortProductsByExpDate: true,
+            });
 
             // APARENTEMENTE O REALM SO CONSULTA O PRIMEIRO REGISTRO DE ARRAY PARA FAZER O 'WHERE'
             // ESSA FUNÇÃO REMOVE QUALQUER VESTIGIO DE LOTES TRATADOS
-            const semTratados = removeAllLotesTratadosFromAllProduts(resultsDB);
+            const results = removeAllLotesTratadosFromAllProduts(allProducts);
 
-            // PRIMEIRO PRECISEI PERCORRER TODOS OS RESULTADOS E ORDERNAR CADA LOTE INDIVIDUALMENTE
-            // E COM ISSO RETORNA UM NOVO ARRAY DE OBJETO, PQ NAO ERA POSSIVEL RETORNA O
-            // ANTIGO MODIFICADO
-            const resultsTemp = sortProductsLotesByLotesExpDate(semTratados);
-            // classifica os produtos em geral pelo o mais proximo de vencer
-            // DEPOIS DE TER TODOS OS PRODUTOS COM OS SEUS LOTES ORDENADOS POR VENCIMENTO, SIMPLISMENTE PEGO O
-            // PRIMEIRO LOTE DE CADA PRODUTO(JÁ QUE SEMPRE SERÁ O MAIS PROXIMO A VENCER) E FAÇO A ORDENAÇÃO
-            // DE TODOS OS PRODUTOS BASEADO NESTE PRIMEIRO LOTE
-            const results = sortProductsByFisrtLoteExpDate(resultsTemp);
-
-            if (results.length > 10) {
-                const resultsMin = [];
-                const howManyResultsToShow =
-                    results.length > 20 ? 20 : results.length;
-
-                for (let i = 0; i < howManyResultsToShow; i++) {
-                    resultsMin.push(results[i]);
-                }
-
-                setProducts(resultsMin);
-            } else {
-                setProducts(results);
-            }
+            setProducts(results);
         } catch (err) {
             setError(err.message);
         } finally {
