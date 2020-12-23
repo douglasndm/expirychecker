4ef147e4b7e6e8cb7eae6d4ae97ce32d0330262e
Douglas Nunes de Mattos <DouglasNunesdeMattos@outlook.com>
2020-12-23 20:05:08 -0300

    App now builds on iOS

--- src/Services/BackgroundJobs.ts
+++ src/Services/BackgroundJobs.ts
@@ -1,24 +1,27 @@
 import BackgroundJob from 'react-native-background-job';
+import { Platform } from 'react-native';
 
 import { getAllProductsNextToExp } from '../Functions/ProductsNotifications';
 
-// REGISTRA O SERVIÇO QUE VAI RODAR AS NOTIFICAÇÕES
-const backgroundJob = {
-    jobKey: 'backgroundNotification',
-    job: () => {
-        getAllProductsNextToExp();
-    },
-};
-BackgroundJob.register(backgroundJob);
+if (Platform.OS === 'android') {
+    // REGISTRA O SERVIÇO QUE VAI RODAR AS NOTIFICAÇÕES
+    const backgroundJob = {
+        jobKey: 'backgroundNotification',
+        job: () => {
+            getAllProductsNextToExp();
+        },
+    };
+    BackgroundJob.register(backgroundJob);
 
-const backgroundSchedule = {
-    jobKey: 'backgroundNotification',
-    period: __DEV__ ? 900000 : 86400000,
-    allowExecutionInForeground: true,
-};
+    const backgroundSchedule = {
+        jobKey: 'backgroundNotification',
+        period: __DEV__ ? 900000 : 86400000,
+        allowExecutionInForeground: true,
+    };
 
-BackgroundJob.schedule(backgroundSchedule)
-    .then(() => console.log('Success'))
-    .catch((err) => {
-        throw new Error(err);
-    });
+    BackgroundJob.schedule(backgroundSchedule)
+        .then(() => console.log('Success'))
+        .catch((err) => {
+            throw new Error(err);
+        });
+}
