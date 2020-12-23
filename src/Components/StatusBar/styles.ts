afc1c4da3425ddbd73a5272c4a16643bb97e755c
Douglas Nunes de Mattos <DouglasNunesdeMattos@outlook.com>
2020-12-23 20:30:57 -0300

    fix: status bar background on ios

--- src/Components/StatusBar/styles.ts
+++ src/Components/StatusBar/styles.ts
@@ -1,5 +1,8 @@
 import styled from 'styled-components/native';
+import { Platform, StatusBar } from 'react-native';
 
 export const Bar = styled.StatusBar.attrs((props) => ({
     backgroundColor: props.theme.colors.accent,
-}))``;
+}))`
+    height: ${Platform.OS === 'ios' ? 20 : StatusBar.currentHeight}px;
+`;
