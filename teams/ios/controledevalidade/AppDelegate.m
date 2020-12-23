265fae7006ac6ab50d4e549339123f5531c6b72c
Douglas Nunes de Mattos <DouglasNunesdeMattos@outlook.com>
2020-12-23 20:05:08 -0300

    App now builds on iOS

--- teams/ios/controledevalidade/AppDelegate.m
+++ teams/ios/controledevalidade/AppDelegate.m
@@ -1,10 +1,12 @@
 #import "AppDelegate.h"
 
+#import <Firebase.h>
+
 #import <React/RCTBridge.h>
 #import <React/RCTBundleURLProvider.h>
 #import <React/RCTRootView.h>
 
-#if DEBUG
+#ifdef FB_SONARKIT_ENABLED
 #import <FlipperKit/FlipperClient.h>
 #import <FlipperKitLayoutPlugin/FlipperKitLayoutPlugin.h>
 #import <FlipperKitUserDefaultsPlugin/FKUserDefaultsPlugin.h>
@@ -27,7 +29,11 @@ static void InitializeFlipper(UIApplication *application) {
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
-#if DEBUG
+    if ([FIRApp defaultApp] == nil) {
+        [FIRApp configure];
+    }
+
+#ifdef FB_SONARKIT_ENABLED
   InitializeFlipper(application);
 #endif
 
