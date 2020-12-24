aebfcab587d5fe8ad15a94e5d7ec1c52136b462a
Douglas Nunes de Mattos <DouglasNunesdeMattos@outlook.com>
2020-12-23 22:20:33 -0300

    fix: status bar for all pages on ios and fixing some fonts problems

--- teams/src/Views/About/styles.ts
+++ teams/src/Views/About/styles.ts
@@ -1,8 +1,9 @@
 import styled from 'styled-components/native';
+import { Platform } from 'react-native';
 
 export const Container = styled.View`
     flex: 1;
-    padding: 16px 10px;
+    padding: ${Platform.OS === 'ios' ? 50 : 16}px 10px 5px 10px;
     background: ${(props) => props.theme.colors.background};
 `;
 
