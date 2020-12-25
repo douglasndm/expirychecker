5aff6a144e8711ced81d83d2908f50f6eed693ec
Douglas Nunes de Mattos <DouglasNunesdeMattos@outlook.com>
2020-12-24 22:36:54 -0300

    fix: android/ios permission for camera was not asked

--- teams/src/Components/BarCodeReader/styles.ts
+++ teams/src/Components/BarCodeReader/styles.ts
@@ -1,7 +1,7 @@
 import styled from 'styled-components/native';
 import { CameraKitCameraScreen } from 'react-native-camera-kit';
 
-export const Container = styled.View`
+export const Container = styled.SafeAreaView`
     flex: 1;
     background: ${(props) => props.theme.colors.background};
 `;
@@ -20,3 +20,15 @@ export const Camera = styled(CameraKitCameraScreen).attrs((props) => ({
     frameColor: props.theme.colors.accent,
     laserColor: props.theme.colors.accent,
 }))``;
+
+export const AuthorizationCameraContainer = styled.View`
+    flex: 1;
+    justify-content: center;
+    align-items: center;
+`;
+
+export const AuthorizationCameraText = styled.Text`
+    font-size: 22px;
+    color: ${(props) => props.theme.colors.text};
+    margin: 0 15px;
+`;
