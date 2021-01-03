2d262c8d1515c9b61859b01b8d6d51b085236a7c
Douglas Nunes de Mattos <DouglasNunesdeMattos@outlook.com>
2021-01-03 20:17:23 -0300

    fix: status bar height on android

--- src/Components/StatusBar/styles.ts
+++ src/Components/StatusBar/styles.ts
@@ -1,8 +1,13 @@
-import styled from 'styled-components/native';
+import styled, { css } from 'styled-components/native';
 import { Platform, StatusBar } from 'react-native';
 
-export const Bar = styled.StatusBar.attrs((props) => ({
+export const Bar = styled(StatusBar).attrs((props) => ({
     backgroundColor: props.theme.colors.accent,
 }))`
-    height: ${Platform.OS === 'ios' ? 20 : StatusBar.currentHeight}px;
+    height: 200px;
+
+    ${Platform.OS === 'ios' &&
+    css`
+        height: 20px;
+    `}
 `;
