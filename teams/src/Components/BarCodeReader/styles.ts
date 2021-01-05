4ea1bb3096643d4e80c02491c81bbb3d32c8ad17
Douglas Nunes de Mattos <DouglasNunesdeMattos@outlook.com>
2021-01-05 19:54:45 -0300

    Moving to react-native-qrcode-scanner for Bar Code Reader

--- teams/src/Components/BarCodeReader/styles.ts
+++ teams/src/Components/BarCodeReader/styles.ts
@@ -1,5 +1,5 @@
 import styled from 'styled-components/native';
-import { CameraKitCameraScreen } from 'react-native-camera-kit';
+import QRCodeScanner from 'react-native-qrcode-scanner';
 
 export const Container = styled.SafeAreaView`
     flex: 1;
@@ -16,10 +16,15 @@ export const CameraContainer = styled.View`
     flex: 1;
 `;
 
-export const Camera = styled(CameraKitCameraScreen).attrs((props) => ({
-    frameColor: props.theme.colors.accent,
-    laserColor: props.theme.colors.accent,
-    colorForScannerFrame: props.theme.colors.accent,
+export const Camera = styled(QRCodeScanner).attrs((props) => ({
+    showMarker: true,
+    cameraType: 'back',
+    fadeIn: true,
+    markerStyle: {
+        borderColor: props.theme.colors.accent,
+        height: 125,
+        width: 300,
+    },
 }))``;
 
 export const AuthorizationCameraContainer = styled.View`
