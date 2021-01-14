0f0efddbf0f1b425958cb8dc2a18e05f2dc5652a
Douglas Nunes de Mattos <DouglasNunesdeMattos@outlook.com>
2021-01-13 23:15:34 -0300

    Removing problematic lib to use Picker lib

--- teams/src/Views/Settings/Components/Notifications/styles.ts
+++ teams/src/Views/Settings/Components/Notifications/styles.ts
@@ -1,4 +1,5 @@
 import styled from 'styled-components/native';
+import { Picker as ReactPicker } from '@react-native-picker/picker';
 
 export const Container = styled.View``;
 
@@ -11,4 +12,12 @@ export const SettingNotificationDescription = styled.Text`
     font-size: 14px;
     color: ${(props) => props.theme.colors.text};
     margin-bottom: 10px;
+`;
+
+export const Picker = styled(ReactPicker).attrs((props) => ({
+    itemStyle: {
+        color: props.theme.colors.text,
+    },
+}))`
+    color: ${(props) => props.theme.colors.text};
 `;
