eac5e9cefdd6d31f49f41eb6f49f0150c104969f
Douglas Nunes de Mattos <DouglasNunesdeMattos@outlook.com>
2021-01-14 13:18:21 -0300

    Moving ask review to app root to avoid multiples asks when use navigate in app

--- mobile/src/Views/Home/index.tsx
+++ mobile/src/Views/Home/index.tsx
@@ -14,7 +14,6 @@ import FABProducts from '../../Components/FABProducts';
 import Notification from '../../Components/Notification';
 import ListProducts from '../../Components/ListProducts';
 import BarCodeReader from '../../Components/BarCodeReader';
-import AskReview from '../../Components/AskReview';
 
 import {
     Container,
@@ -142,8 +141,6 @@ const Home: React.FC = () => {
                         />
                     )}
                     <FABProducts />
-
-                    <AskReview />
                 </Container>
             )}
         </>
