8d295425ba342d7cd2ace272f3a2f213071c5458
Douglas Nunes de Mattos <DouglasNunesdeMattos@outlook.com>
2021-01-21 20:57:45 -0300

    Removing settings schema

--- mobile/src/Services/Realm.ts
+++ mobile/src/Services/Realm.ts
@@ -2,11 +2,10 @@ import Realm from 'realm';
 
 import ProductSchema from '../Schemas/ProductSchema';
 import LoteSchema from '../Schemas/LoteSchema';
-import SettingSchema from '../Schemas/SettingSchema';
 
 export default async function RealmInstance(): Promise<Realm> {
     const realm = await Realm.open({
-        schema: [ProductSchema, LoteSchema, SettingSchema],
+        schema: [ProductSchema, LoteSchema],
         schemaVersion: 4,
     });
 
