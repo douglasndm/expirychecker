6816eaa3321d8d68be8f46685965da5cca9aa19d
Douglas Nunes de Mattos <DouglasNunesdeMattos@outlook.com>
2021-01-25 20:09:50 -0300

    This try to fix codePush auto rollback on iOS

--- teams/index.js
+++ teams/index.js
@@ -3,9 +3,16 @@
  */
 
 import { AppRegistry } from 'react-native';
-import App from './src';
+import CodePush from 'react-native-code-push';
+
 import { name as appName } from './app.json';
+import App from './src';
 import './src/Services/BackgroundJobs';
 import './src/Functions/OpenAppTimes';
 
-AppRegistry.registerComponent(appName, () => App);
+const codePushOptions = {
+    checkFrequency: CodePush.CheckFrequency.ON_APP_RESUME,
+    mandatoryInstallMode: CodePush.InstallMode.ON_NEXT_RESUME,
+};
+
+AppRegistry.registerComponent(appName, () => CodePush(codePushOptions)(App));
