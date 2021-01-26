7e1a17c52c8f7cdc219cc006a5eb1117fe285789
Douglas Nunes de Mattos <DouglasNunesdeMattos@outlook.com>
2021-01-25 21:22:46 -0300

    Adding category list view

--- src/Services/Realm.ts
+++ src/Services/Realm.ts
@@ -1,12 +1,13 @@
 import Realm from 'realm';
 
-import ProductSchema from '../Schemas/ProductSchema';
-import LoteSchema from '../Schemas/LoteSchema';
+import ProductSchema from '~/Schemas/ProductSchema';
+import LoteSchema from '~/Schemas/LoteSchema';
+import CategorySchema from '~/Schemas/Category';
 
 export default async function RealmInstance(): Promise<Realm> {
     const realm = await Realm.open({
-        schema: [ProductSchema, LoteSchema],
-        schemaVersion: 4,
+        schema: [ProductSchema, LoteSchema, CategorySchema],
+        schemaVersion: 5,
     });
 
     return realm;
---
+++ src/Views/Category/Edit/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { View } from 'react-native';
+
+// import { Container } from './styles';
+
+const Edit: React.FC = () => {
+    const handleDeleteCategory = useCallback(async () => {}, []);
+
+    return <View />;
+};
+
+export default Edit;
