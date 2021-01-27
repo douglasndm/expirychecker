1b4493419acac01574002308209adfbec4e4915a
Douglas Nunes de Mattos <DouglasNunesdeMattos@outlook.com>
2021-01-26 22:04:01 -0300

    Test: using a single fixed ad in home and not a lot anymore

--- src/Components/ListProducts/ProductContainer/index.tsx
+++ src/Components/ListProducts/ProductContainer/index.tsx
@@ -1,8 +1,5 @@
-import React, { useCallback, useContext, useMemo, useState } from 'react';
-import { Platform, PixelRatio } from 'react-native';
+import React, { useCallback, useContext, useMemo } from 'react';
 import { useNavigation } from '@react-navigation/native';
-import { BannerAd, BannerAdSize, TestIds } from '@react-native-firebase/admob';
-import EnvConfig from 'react-native-config';
 import { addDays, isPast } from 'date-fns';
 
 import { translate } from '~/Locales';
@@ -27,8 +24,6 @@ const ProductContainer: React.FC<RequestProps> = ({
     const { userPreferences } = useContext(PreferencesContext);
     const { navigate } = useNavigation();
 
-    const [adFailed, setAdFailed] = useState(false);
-
     const expired = useMemo(() => {
         return product.lotes[0] && isPast(product.lotes[0].exp_date);
     }, [product.lotes]);
@@ -41,32 +36,13 @@ const ProductContainer: React.FC<RequestProps> = ({
         );
     }, [userPreferences.howManyDaysToBeNextToExpire, product.lotes]);
 
-    const adUnitId = useMemo(() => {
-        if (__DEV__) {
-            return TestIds.BANNER;
-        }
-
-        return Platform.OS === 'ios'
-            ? EnvConfig.IOS_ADUNIT_BANNER_BETWEEN_PRODUCTS_LIST
-            : EnvConfig.ANDROID_ADMOB_ADUNITID_BETWEENPRODUCTS;
-    }, []);
-
-    const bannerSize = useMemo(() => {
-        if (PixelRatio.get() >= 3) {
-            return BannerAdSize.ADAPTIVE_BANNER;
-        }
-
-        return BannerAdSize.LARGE_BANNER;
-    }, []);
-
     const showAd = useMemo(() => {
         if (disableAds) return false;
         if (userPreferences.isUserPremium) return false;
-        if (adFailed) return false;
         if (index === 0) return false;
         if (index && index % 5 === 0) return true;
         return false;
-    }, [disableAds, userPreferences.isUserPremium, index, adFailed]);
+    }, [disableAds, userPreferences.isUserPremium, index]);
 
     const handleNavigateToProPage = useCallback(() => {
         navigate('Pro');
@@ -80,12 +56,6 @@ const ProductContainer: React.FC<RequestProps> = ({
         <Container>
             {showAd && (
                 <AdView>
-                    <BannerAd
-                        unitId={adUnitId}
-                        size={bannerSize}
-                        onAdFailedToLoad={() => setAdFailed(true)}
-                    />
-
                     <ButtonPro onPress={handleNavigateToProPage}>
                         <ButtonProText>
                             {translate(`ProBanner_Text${choosenAdText}`)}
