cb7ca16dde40df7a1ca52f0baffba653a0acb7af
Douglas Nunes de Mattos <DouglasNunesdeMattos@outlook.com>
2021-03-21 12:08:52 -0300

    Removing user info from menu

--- teams/src/Components/DrawerMenu/styles.ts
+++ teams/src/Components/DrawerMenu/styles.ts
@@ -3,9 +3,25 @@ import { Drawer } from 'react-native-paper';
 import Ionicons from 'react-native-vector-icons/Ionicons';
 import { RectButton } from 'react-native-gesture-handler';
 
+import LogoImg from '~/Assets/Logo.png';
+
 export const Container = styled.View`
     flex: 1;
     background-color: ${({ theme }) => theme.colors.background};
+`;
+
+export const LogoContainer = styled.View`
+    margin-top: -10px;
+    background-color: ${props => props.theme.colors.accent};
+    align-items: center;
+`;
+
+export const Logo = styled.Image.attrs(() => ({
+    source: LogoImg,
+    resizeMode: 'cover',
+}))`
+    width: 120px;
+    height: 120px;
 `;
 
 export const MenuItemContainer = styled(RectButton)`
@@ -22,7 +38,7 @@ export const MenuContent = styled.View`
 
 export const MenuItemText = styled.Text`
     margin-left: 20px;
-    color: ${(props) => props.theme.colors.text};
+    color: ${props => props.theme.colors.text};
 `;
 
 export const Icons = styled(Ionicons).attrs(() => ({
