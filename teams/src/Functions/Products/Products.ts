5cc3feb6040ebdd110514abb75e3e6b19ad64200
Douglas Nunes de Mattos <DouglasNunesdeMattos@outlook.com>
2021-05-08 23:56:22 -0300

    Home now will users logged user products

--- teams/src/Functions/Products/Products.ts
+++ teams/src/Functions/Products/Products.ts
@@ -1,5 +1,7 @@
 import API from '~/Services/API';
 
+import { getUserSession } from '~/Functions/Auth/Login';
+
 interface getAllProductsProps {
     team_id: string;
 }
@@ -8,8 +10,20 @@ export async function getAllProducts({
     team_id,
 }: getAllProductsProps): Promise<Array<IProduct>> {
     try {
+        const userSession = await getUserSession();
+        const token = userSession?.token;
+
+        if (!token) {
+            throw new Error('Token is missing');
+        }
+
         const response = await API.get<IAllTeamProducts>(
-            `/team/${team_id}/products`
+            `/team/${team_id}/products`,
+            {
+                headers: {
+                    Authorization: `Bearer ${token}`,
+                },
+            }
         );
 
         return response.data.products;
--- teams/src/Services/API.ts
+++ teams/src/Services/API.ts
@@ -1,13 +1,7 @@
 import axios from 'axios';
 
-const token =
-    'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjQ1ZWFkYjMwLTVkNmEtNDA2YS1hNjJkLTZkMzVhYjA1YjgzOSIsImlhdCI6MTYyMDM1ODE0MCwiZXhwIjoxNjIwNDQ0NTQwfQ.yTEmfs-b_Sk44X2T-H8-ZuqBh2IXtKz0btvkJYZjYeE';
-
 const api = axios.create({
     baseURL: 'http://127.0.0.1:3213',
-    headers: {
-        Authorization: `Bearer ${token}`,
-    },
 });
 
 export default api;
