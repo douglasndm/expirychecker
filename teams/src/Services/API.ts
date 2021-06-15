8bb149fd7ae255e66e58f6c2cea3d671f570a608
Douglas Nunes de Mattos <DouglasNunesdeMattos@outlook.com>
2021-06-15 00:09:53 -0300

    adding user token on all request automatically

--- teams/src/Services/API.ts
+++ teams/src/Services/API.ts
@@ -1,6 +1,7 @@
 import axios from 'axios';
 import EnvConfig from 'react-native-config';
 import messaging from '@react-native-firebase/messaging';
+import auth from '@react-native-firebase/auth';
 
 const api = axios.create({
     baseURL: __DEV__ ? 'http://192.168.1.3:3213' : EnvConfig.API_URL,
@@ -9,6 +10,14 @@ const api = axios.create({
 api.interceptors.request.use(async config => {
     const token = await messaging().getToken();
     config.headers.deviceid = token;
+
+    // check if user is signed and if it is add token to all future axios requests
+    const { currentUser } = auth();
+    const authToken = await currentUser?.getIdTokenResult();
+
+    if (authToken) {
+        config.headers.Authorization = `Bearer ${authToken.token}`;
+    }
 
     return config;
 });
