f5f7e5960da21af06642e100c893de171196c430
Douglas Nunes de Mattos <DouglasNunesdeMattos@outlook.com>
2021-06-15 16:15:25 -0300

    Import/Export now works with categories

--- mobile/src/Functions/Backup.ts
+++ mobile/src/Functions/Backup.ts
@@ -18,6 +18,8 @@ import { translate } from '../Locales';
 import { createProduct } from './Product';
 import { getAllProducts } from './Products';
 import { getAllCategories } from './Category';
+import { saveMany } from './Products/index';
+import { saveManyCategories } from './Categories';
 
 const backupDir = `${DocumentDirectoryPath}/backupDir`;
 
@@ -70,7 +72,7 @@ interface generateBackupFileProps {
 }
 
 export async function generateBackupFile({
-    includeCategories = false,
+    includeCategories = true,
     store,
 }: generateBackupFileProps): Promise<string> {
     if (!(await exists(`${backupDir}`))) {
@@ -145,114 +147,103 @@ export async function generateBackupFile({
 }
 
 export async function exportBackupFile(): Promise<void> {
-    try {
-        await generateBackupFile({});
+    await generateBackupFile({});
 
-        const zipPath = await zip(
-            `${backupDir}`,
-            `${DocumentDirectoryPath}/${translate(
-                'Function_Export_FileName'
-            )}.zip`
-        );
+    const zipPath = await zip(
+        `${backupDir}`,
+        `${DocumentDirectoryPath}/${translate('Function_Export_FileName')}.zip`
+    );
 
-        await Share.open({
-            title: translate('Function_Share_SaveFileTitle'),
-            url: `file://${zipPath}`,
-        });
+    await Share.open({
+        title: translate('Function_Share_SaveFileTitle'),
+        url: `file://${zipPath}`,
+    });
 
-        await unlink(`${backupDir}`);
-    } catch (err) {
-        if (err.message !== 'Error: User did not share') {
-            throw new Error(err.message);
-        }
-    }
+    await unlink(`${backupDir}`);
 }
 
 export async function importBackupFile(): Promise<void> {
-    try {
-        const options = {
-            type: Platform.OS === 'ios' ? 'public.item' : '*/*',
-            copyTo: 'documentDirectory',
-        };
+    const options = {
+        type: Platform.OS === 'ios' ? 'public.item' : '*/*',
+        copyTo: 'documentDirectory',
+    };
 
-        const filePicked = await DocumentPicker.pick(options);
+    const filePicked = await DocumentPicker.pick(options);
 
-        // Separa o nome do arquivo da extensão para fazer a validação da extensão do arquivo
-        const [, extension] = filePicked.name.split('.');
+    // Separa o nome do arquivo da extensão para fazer a validação da extensão do arquivo
+    const [, extension] = filePicked.name.split('.');
 
-        // caso a extensão do arquivo não for cvbf lança um erro e sai da função
-        if (extension !== 'cvbf' && extension !== 'zip') {
-            throw new Error(translate('Function_Import_Error_InvalidExtesion'));
-        }
-
-        let backupFilePath = null;
-
-        if (extension === 'zip') {
-            if (!(await exists(backupDir))) {
-                await mkdir(backupDir);
-            }
-
-            let filePath = null;
-
-            if (Platform.OS === 'android') {
-                filePath = await RNGRP.getRealPathFromURI(filePicked.uri);
-            } else {
-                filePath = filePicked.fileCopyUri;
-            }
-
-            await unzip(filePath, backupDir);
-
-            if (await exists(`${backupDir}/backupImages.zip`)) {
-                await unzip(
-                    `${backupDir}/backupImages.zip`,
-                    `${DocumentDirectoryPath}/images`
-                );
-            }
-
-            const dir = await readDir(backupDir);
-            const backupFile = dir.find(item => {
-                const [, ext] = item.name.split('.');
-
-                if (ext === 'cvbf') return true;
-                return false;
-            });
-
-            if (backupFile?.name) {
-                backupFilePath = `${backupDir}/${backupFile.name}`;
-            }
-        }
-        if (extension === 'cvbf') {
-            backupFilePath = filePicked.fileCopyUri;
-        }
-
-        if (!backupFilePath) {
-            throw new Error('Extesion is not valid');
-        }
-
-        // pega o arquivo temporario gerado pelo filePicker e faz a leitura dele
-        const fileRead = await RNFS.readFile(backupFilePath);
-
-        // decriptografa o arquivo lido
-        const decryptedFile = CryptoJS.AES.decrypt(
-            fileRead,
-            EnvConfig.APPLICATION_SECRET_BACKUP_CRYPT
-        );
-
-        // converte o arquivo em formato de bytes puros para string
-        const originalFile = decryptedFile.toString(CryptoJS.enc.Utf8);
-
-        // converte tudo de novo para json
-        const products = JSON.parse(originalFile);
-
-        for (const p of products) {
-            await createProduct({ product: p, ignoreDuplicate: true });
-        }
-
-        await unlink(backupDir);
-    } catch (err) {
-        console.log(err);
-        throw new Error(err);
+    // caso a extensão do arquivo não for cvbf lança um erro e sai da função
+    if (extension !== 'cvbf' && extension !== 'zip') {
+        throw new Error(translate('Function_Import_Error_InvalidExtesion'));
     }
+
+    let backupFilePath = null;
+
+    if (extension === 'zip') {
+        if (!(await exists(backupDir))) {
+            await mkdir(backupDir);
+        }
+
+        let filePath = null;
+
+        if (Platform.OS === 'android') {
+            filePath = await RNGRP.getRealPathFromURI(filePicked.uri);
+        } else {
+            filePath = filePicked.fileCopyUri;
+        }
+
+        await unzip(filePath, backupDir);
+
+        if (await exists(`${backupDir}/backupImages.zip`)) {
+            await unzip(
+                `${backupDir}/backupImages.zip`,
+                `${DocumentDirectoryPath}/images`
+            );
+        }
+
+        const dir = await readDir(backupDir);
+        const backupFile = dir.find(item => {
+            const [, ext] = item.name.split('.');
+
+            if (ext === 'cvbf') return true;
+            return false;
+        });
+
+        if (backupFile?.name) {
+            backupFilePath = `${backupDir}/${backupFile.name}`;
+        }
+    }
+    if (extension === 'cvbf') {
+        backupFilePath = filePicked.fileCopyUri;
+    }
+
+    if (!backupFilePath) {
+        throw new Error('Extesion is not valid');
+    }
+
+    // pega o arquivo temporario gerado pelo filePicker e faz a leitura dele
+    const fileRead = await RNFS.readFile(backupFilePath);
+
+    // decriptografa o arquivo lido
+    const decryptedFile = CryptoJS.AES.decrypt(
+        fileRead,
+        EnvConfig.APPLICATION_SECRET_BACKUP_CRYPT
+    );
+
+    // converte o arquivo em formato de bytes puros para string
+    const originalFile = decryptedFile.toString(CryptoJS.enc.Utf8);
+
+    // converte tudo de novo para json
+    const parsedFile = JSON.parse(originalFile);
+
+    const { categories } = parsedFile;
+    const { products } = parsedFile;
+
+    await saveManyCategories(categories);
+    await saveMany(products);
+
+    await unlink(backupDir);
 }
 interface IProductImage {
     productId: number;
