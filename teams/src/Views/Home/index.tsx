c087ed4b4f7f9d5ccaed914cc23d358073123e02
Douglas Nunes de Mattos <DouglasNunesdeMattos@outlook.com>
2021-07-04 22:19:17 -0300

    Version bump 0.11

--- teams/src/Views/Home/index.tsx
+++ teams/src/Views/Home/index.tsx
@@ -115,7 +115,7 @@ const Home: React.FC = () => {
             ) : (
                 <Container>
                     {/* <Header title={preferences.selectedTeam.team.name} /> */}
-                    <Header title="Beta 10" />
+                    <Header title="Beta 11" />
 
                     {products.length > 0 && (
                         <InputTextContainer>
