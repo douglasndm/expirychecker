90e055299dddf59ea48acdb158b3d1d74b19f588
Douglas Nunes de Mattos <DouglasNunesdeMattos@outlook.com>
2021-07-17 18:08:55 -0300

    Adding a option to edit store name

--- src/Views/Stores/Edit/styles.ts
+++ src/Views/Stores/Edit/styles.ts
@@ -1,18 +1,20 @@
 import styled from 'styled-components/native';
-import { Button } from 'react-native-paper';
 import Ionicons from 'react-native-vector-icons/Ionicons';
+import { Button } from 'react-native-paper';
 
 export const Container = styled.View`
     flex: 1;
-    background: ${props => props.theme.colors.background};
+    background-color: ${({ theme }) => theme.colors.background};
 `;
 
-export const HeaderContainer = styled.View``;
+export const Content = styled.View`
+    margin: 20px 10px 0;
+`;
 
 export const ActionsButtonContainer = styled.View`
-    flex-direction: column;
-    align-items: flex-start;
-    margin-top: 5px;
+    flex-direction: row;
+    justify-content: center;
+    margin-top: 10px;
 `;
 
 export const ButtonPaper = styled(Button).attrs(props => ({
