cabb57005b95257cc8053014c4b70f252a164f5d
Douglas Nunes de Mattos <douglasnunesdemattos@outlook.com>
2021-09-06 18:38:53 -0300

    fix: AppsFlyer for iOS

--- src/Services/AppsFlyer.ts
+++ src/Services/AppsFlyer.ts
@@ -4,4 +4,6 @@ import dotenv from 'react-native-config';
 appsFlyer.initSdk({
     devKey: dotenv.APPSFLYER_KEY,
     isDebug: __DEV__,
+    appId: dotenv.IOS_APP_ID,
+    timeToWaitForATTUserAuthorization: 15,
 });
