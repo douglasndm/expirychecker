65220227ad4d66e7afb6320c4f78abf46640fab9
Douglas Nunes de Mattos <douglasnunesdemattos@outlook.com>
2021-10-04 19:00:35 -0300

    Adding Categories export for Excel

--- src/Functions/Excel.ts
+++ src/Functions/Excel.ts
@@ -8,21 +8,6 @@ import { shareFile } from './Share';
 import { getAllProducts } from './Products';
 import { getStore } from './Stores';
 
-interface ExcelRowProps {
-    produto: string;
-    codigo: string;
-    loja: string;
-    lote: string;
-    vencimento: Date;
-    quantidade: number;
-    preço: number;
-}
-
-interface exportModel {
-    product: Omit<IProduct, 'Lotes'>;
-    batch: ILote;
-}
-
 function sortProducts(products: Array<exportModel>): Array<exportModel> {
     const sorted: Array<exportModel> = [];
 
@@ -38,87 +23,93 @@ function sortProducts(products: Array<exportModel>): Array<exportModel> {
     return sorted;
 }
 
-interface exportProps {
-    sortBy: 'created_date' | 'expire_date';
-}
-
-export async function exportToExcel({ sortBy }: exportProps): Promise<void> {
+export async function exportToExcel({
+    sortBy,
+    category,
+}: exportProps): Promise<void> {
     let dateFormat = 'dd/MM/yyyy';
 
     if (getLocales()[0].languageCode === 'en') {
         dateFormat = 'MM/dd/yyyy';
     }
 
-    try {
-        const excelExport: Array<exportModel> = [];
+    const excelExport: Array<exportModel> = [];
 
-        const workbook = XLSX.utils.book_new();
+    const workbook = XLSX.utils.book_new();
 
-        const allProducts = await getAllProducts({});
+    const allProducts = await getAllProducts({});
 
-        allProducts.forEach(p => {
-            p.lotes.forEach(l => {
-                excelExport.push({
-                    product: p,
-                    batch: l,
-                });
+    allProducts.forEach(p => {
+        p.lotes.forEach(l => {
+            excelExport.push({
+                product: p,
+                batch: l,
             });
         });
+    });
 
-        let sortedProducts;
+    let sortedProducts;
 
-        if (sortBy === 'expire_date') {
-            sortedProducts = sortProducts(excelExport);
-        } else {
-            sortedProducts = excelExport;
-        }
-
-        const excelRows: Array<ExcelRowProps> = [];
-
-        for (const item of sortedProducts) {
-            const store = await getStore(item.product.store || '');
-
-            const row: any = {};
-
-            row[strings.Function_Excel_ColumnName_ProductName] =
-                item.product.name;
-            row[strings.Function_Excel_ColumnName_ProductCode] =
-                item.product.code || '';
-            row[strings.Function_Excel_ColumnName_ProductStore] = store?.name;
-            row[strings.Function_Excel_ColumnName_BatchName] = item.batch.lote;
-            row[strings.Function_Excel_ColumnName_BatchPrice] =
-                item.batch.price || 0;
-            row[strings.Function_Excel_ColumnName_BatchAmount] =
-                item.batch.amount || 0;
-            row[strings.Function_Excel_ColumnName_BatchExpDate] = format(
-                item.batch.exp_date,
-                dateFormat
-            );
-            row.Tratado = item.batch.status === 'Tratado' ? 'Sim' : 'Não';
-
-            excelRows.push(row);
-        }
-
-        const worksheet = XLSX.utils.json_to_sheet(excelRows);
-
-        XLSX.utils.book_append_sheet(
-            workbook,
-            worksheet,
-            strings.Function_Excel_Workbook_Name
-        );
-
-        const wbout = XLSX.write(workbook, {
-            type: 'base64',
-            bookType: 'xlsx',
-        });
-
-        await shareFile({
-            fileAsString: wbout,
-            fileName: strings.Function_Excel_filename,
-            fileExtesion: 'xlsx',
-            encoding: 'base64',
-        });
-    } catch (err) {
-        throw new Error(err);
+    if (sortBy === 'expire_date') {
+        sortedProducts = sortProducts(excelExport);
+    } else {
+        sortedProducts = excelExport;
     }
+
+    if (category && category !== 'null') {
+        sortedProducts = sortedProducts.filter(prod => {
+            const findedCate = prod.product.categories.filter(
+                c => c === category
+            );
+
+            if (findedCate.length > 0) {
+                return true;
+            }
+            return false;
+        });
+    }
+    const excelRows: Array<ExcelRowProps> = [];
+
+    for (const item of sortedProducts) {
+        const store = await getStore(item.product.store || '');
+
+        const row: any = {};
+
+        row[strings.Function_Excel_ColumnName_ProductName] = item.product.name;
+        row[strings.Function_Excel_ColumnName_ProductCode] =
+            item.product.code || '';
+        row[strings.Function_Excel_ColumnName_ProductStore] = store?.name;
+        row[strings.Function_Excel_ColumnName_BatchName] = item.batch.lote;
+        row[strings.Function_Excel_ColumnName_BatchPrice] =
+            item.batch.price || 0;
+        row[strings.Function_Excel_ColumnName_BatchAmount] =
+            item.batch.amount || 0;
+        row[strings.Function_Excel_ColumnName_BatchExpDate] = format(
+            item.batch.exp_date,
+            dateFormat
+        );
+        row.Tratado = item.batch.status === 'Tratado' ? 'Sim' : 'Não';
+
+        excelRows.push(row);
+    }
+
+    const worksheet = XLSX.utils.json_to_sheet(excelRows);
+
+    XLSX.utils.book_append_sheet(
+        workbook,
+        worksheet,
+        strings.Function_Excel_Workbook_Name
+    );
+
+    const wbout = XLSX.write(workbook, {
+        type: 'base64',
+        bookType: 'xlsx',
+    });
+
+    await shareFile({
+        fileAsString: wbout,
+        fileName: strings.Function_Excel_filename,
+        fileExtesion: 'xlsx',
+        encoding: 'base64',
+    });
 }
