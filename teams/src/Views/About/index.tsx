1cd0c99fec82208706a9615bc97962b7aa2734c4
Douglas Nunes de Mattos <douglasnunesdemattos@outlook.com>
2021-10-16 18:52:25 -0300

    Updating About page

--- teams/src/Views/About/index.tsx
+++ teams/src/Views/About/index.tsx
@@ -1,25 +1,40 @@
-import React, { useCallback } from 'react';
+import React, { useState, useCallback } from 'react';
 import { View, Linking } from 'react-native';
-import { getVersion } from 'react-native-device-info';
+import { useNavigation } from '@react-navigation/native';
+import {
+    getVersion,
+    getBuildNumber,
+    getSystemName,
+    getSystemVersion,
+} from 'react-native-device-info';
+import messaging from '@react-native-firebase/messaging';
+import Purchases from 'react-native-purchases';
 
 import strings from '~/Locales';
 
 import StatusBar from '~/Components/StatusBar';
-import Header from '~/Components/Header';
+import BackButton from '~/Components/BackButton';
 
 import {
     Container,
     Content,
+    PageTitle,
     ApplicationVersion,
     AboutSection,
     ApplicationName,
     Text,
     Link,
+    SocialContainer,
+    SocialIcon,
 } from './styles';
 
 const About: React.FC = () => {
-    const navigateToTelegram = useCallback(async () => {
-        await Linking.openURL('https://t.me/douglasdev');
+    const { goBack } = useNavigation();
+
+    const [tapsCount, setTapsCount] = useState<number>(0);
+
+    const handleNavigateToSite = useCallback(async () => {
+        await Linking.openURL('https://douglasndm.dev');
     }, []);
 
     const navigateToTerms = useCallback(async () => {
@@ -34,58 +49,101 @@ const About: React.FC = () => {
         await Linking.openURL('https://www.linkedin.com/in/douglasndm/');
     }, []);
 
+    const handleNaviTwitter = useCallback(async () => {
+        await Linking.openURL('https://www.twitter.com/douglasndmdev/');
+    }, []);
+
+    const handleNaviFB = useCallback(async () => {
+        await Linking.openURL('https://www.facebook.com/douglasndmdev/');
+    }, []);
+
+    const handleNaviMail = useCallback(async () => {
+        await Linking.openURL('mailto:suporte@douglasndm.dev');
+    }, []);
+
     const handleFlatIconPress = useCallback(async () => {
         await Linking.openURL('https://www.flaticon.com/authors/srip');
     }, []);
 
+    const handleShareIdInfo = useCallback(async () => {
+        setTapsCount(tapsCount + 1);
+        if (tapsCount > 15) {
+            const revenueCatId = await Purchases.getAppUserID();
+            const token = await messaging().getToken();
+
+            const userInfo = {
+                purchase_idetinfy: revenueCatId,
+                firebase_messaging: token,
+            };
+            // shareText({
+            //    title: 'User informations',
+            //    text: JSON.stringify(userInfo),
+            // });
+        }
+    }, [tapsCount]);
+
     return (
         <Container>
             <StatusBar />
-            <Header title={strings.View_About_PageTitle} noDrawer />
-
             <Content>
-                <AboutSection>
-                    <ApplicationName>{strings.AppName}</ApplicationName>
-
-                    <ApplicationVersion>
-                        {strings.View_About_AppVersion + getVersion()}
-                    </ApplicationVersion>
-                </AboutSection>
-
-                <AboutSection>
-                    <Text>
-                        {strings.BeforeTermsAndPrivacy}
-                        <Link onPress={navigateToTerms}>{strings.Terms}</Link>
-                        {strings.BetweenTermsAndPrivacy}
-                        <Link onPress={navigateToPrivacy}>
-                            {strings.PrivacyPolicy}
-                        </Link>
-                        .
-                    </Text>
-                </AboutSection>
-
-                <AboutSection>
-                    <Text>{strings.View_About_DevelopedBy}</Text>
-                    <Link onPress={handleLinkedinPress}>Linkedin</Link>
-                </AboutSection>
-
-                <AboutSection>
-                    <Text>{strings.View_About_NeedHelp}</Text>
-                    <Link onPress={navigateToTelegram}>
-                        {strings.View_About_HelpTelegram}
-                    </Link>
-                </AboutSection>
-
-                <AboutSection>
-                    <Text>{strings.View_About_LogoMadeBy}</Text>
-
-                    <View>
-                        <Link onPress={handleFlatIconPress}>
-                            https://www.flaticon.com/authors/srip
-                        </Link>
-                    </View>
-                </AboutSection>
+                <BackButton handleOnPress={goBack} />
+                <PageTitle>{strings.View_About_PageTitle}</PageTitle>
             </Content>
+
+            <AboutSection>
+                <ApplicationName>{strings.AppName}</ApplicationName>
+
+                <ApplicationVersion>
+                    {`${
+                        strings.View_About_AppVersion
+                    } ${getVersion()} (Build: ${getBuildNumber()})`}
+                </ApplicationVersion>
+                <ApplicationVersion>{`${getSystemName()} ${getSystemVersion()}`}</ApplicationVersion>
+            </AboutSection>
+
+            <AboutSection>
+                <Link onPress={handleNavigateToSite}>
+                    {strings.Menu_Button_KnowOthersApps}
+                </Link>
+            </AboutSection>
+
+            <AboutSection>
+                <Text>
+                    {strings.BeforeTermsAndPrivacy}
+                    <Link onPress={navigateToTerms}>{strings.Terms}</Link>
+                    {strings.BetweenTermsAndPrivacy}
+                    <Link onPress={navigateToPrivacy}>
+                        {strings.PrivacyPolicy}
+                    </Link>
+                    .
+                </Text>
+            </AboutSection>
+
+            <AboutSection>
+                <Text>{strings.View_About_LogoMadeBy}</Text>
+
+                <View>
+                    <Link onPress={handleFlatIconPress}>
+                        https://www.flaticon.com/authors/srip
+                    </Link>
+                </View>
+            </AboutSection>
+
+            <AboutSection>
+                <Text onPress={handleShareIdInfo}>
+                    {strings.View_About_DevelopedBy}
+                </Text>
+            </AboutSection>
+
+            <SocialContainer>
+                <SocialIcon
+                    name="logo-linkedin"
+                    onPress={handleLinkedinPress}
+                />
+                <SocialIcon name="logo-twitter" onPress={handleNaviTwitter} />
+                <SocialIcon name="logo-facebook" onPress={handleNaviFB} />
+                <SocialIcon name="mail-outline" onPress={handleNaviMail} />
+            </SocialContainer>
         </Container>
     );
 };
