008a76e4110d19f0c7c1eb441960abfedacde292
Douglas Nunes de Mattos <douglasnunesdemattos@outlook.com>
2021-11-21 17:59:54 -0300

    Adding delete many products for PRO

--- src/Components/ListProducts/index.tsx
+++ src/Components/ListProducts/index.tsx
@@ -2,8 +2,9 @@ import React, { useCallback, useContext, useEffect, useState } from 'react';
 import { View, FlatList } from 'react-native';
 import { useNavigation } from '@react-navigation/native';
 import { PurchasesPackage } from 'react-native-purchases';
-
+import Dialog from 'react-native-dialog';
 import { showMessage } from 'react-native-flash-message';
+
 import strings from '~/Locales';
 
 import PreferencesContext from '~/Contexts/PreferencesContext';
@@ -19,6 +20,12 @@ import GenericButton from '../Button';
 
 import {
     Container,
+    ActionButtonsContainer,
+    ProductContainer,
+    SelectButtonContainer,
+    SelectButton,
+    SelectIcon,
+    ButtonPaper,
     ProBanner,
     ProText,
     CategoryDetails,
@@ -29,6 +36,7 @@ import {
     Icons,
 } from './styles';
 import { getDisableAds } from '~/Functions/Settings';
+import { deleteManyProducts } from '~/Utils/Products';
 
 interface RequestProps {
     products: Array<IProduct>;
@@ -46,6 +54,10 @@ const ListProducts: React.FC<RequestProps> = ({
     isRefreshing = false,
 }: RequestProps) => {
     const { navigate } = useNavigation();
+
+    const [selectedProds, setSelectedProds] = useState<Array<number>>([]);
+    const [selectMode, setSelectMode] = useState(false);
+    const [deleteModal, setDeleteModal] = useState(false);
 
     const [noAdsPackage, setNoAdsPackages] = useState<PurchasesPackage | null>(
         null
@@ -88,15 +100,41 @@ const ListProducts: React.FC<RequestProps> = ({
                     });
                 }
             } catch (err) {
-                showMessage({
-                    message: err.message,
-                    type: 'warning',
-                });
+                if (err instanceof Error)
+                    showMessage({
+                        message: err.message,
+                        type: 'warning',
+                    });
             } finally {
                 setIsPurchasing(false);
             }
         }
     }, [noAdsPackage, setUserPreferences, userPreferences]);
+
+    const switchSelectedItem = useCallback(
+        (productId: number) => {
+            const isChecked = selectedProds.find(id => id === productId);
+
+            if (!isChecked) {
+                const prodsIds = [...selectedProds, productId];
+
+                setSelectedProds(prodsIds);
+                return;
+            }
+
+            const newSelected = selectedProds.filter(id => id !== productId);
+            setSelectedProds(newSelected);
+        },
+        [selectedProds]
+    );
+
+    const handleEnableSelectMode = useCallback(() => {
+        if (userPreferences.isUserPremium) setSelectMode(true);
+    }, [userPreferences.isUserPremium]);
+
+    const handleDisableSelectMode = useCallback(() => {
+        setSelectMode(false);
+    }, []);
 
     useEffect(() => {
         loadRemoveAdsData();
@@ -162,12 +200,89 @@ const ListProducts: React.FC<RequestProps> = ({
         return <InvisibleComponent />;
     }, [products.length, isHome, handleNavigateToAllProducts]);
 
-    const renderComponent = useCallback(({ item, index }) => {
-        return <ProductItem product={item} index={index} />;
-    }, []);
+    const renderComponent = useCallback(
+        ({ item, index }) => {
+            const product: IProduct = item as IProduct;
+
+            const isChecked = selectedProds.find(id => id === product.id);
+
+            return (
+                <ProductContainer onLongPress={handleEnableSelectMode}>
+                    {selectMode && (
+                        <SelectButtonContainer>
+                            <SelectButton
+                                onPress={() => switchSelectedItem(product.id)}
+                            >
+                                {isChecked ? (
+                                    <SelectIcon name="checkmark-circle-outline" />
+                                ) : (
+                                    <SelectIcon name="ellipse-outline" />
+                                )}
+                            </SelectButton>
+                        </SelectButtonContainer>
+                    )}
+                    <ProductItem
+                        product={product}
+                        handleEnableSelect={handleEnableSelectMode}
+                    />
+                </ProductContainer>
+            );
+        },
+        [handleEnableSelectMode, selectMode, selectedProds, switchSelectedItem]
+    );
+
+    const handleSwitchDeleteModal = useCallback(() => {
+        setDeleteModal(!deleteModal);
+    }, [deleteModal]);
+
+    const handleDeleteMany = useCallback(async () => {
+        if (selectedProds.length <= 0) {
+            handleDisableSelectMode();
+            setDeleteModal(false);
+            return;
+        }
+        try {
+            await deleteManyProducts({ productsIds: selectedProds });
+
+            if (onRefresh) onRefresh();
+
+            showMessage({
+                message:
+                    strings.ListProductsComponent_ProductsDeleted_Notification,
+                type: 'info',
+            });
+        } catch (err) {
+            if (err instanceof Error)
+                showMessage({
+                    message: err.message,
+                    type: 'danger',
+                });
+        }
+    }, [handleDisableSelectMode, onRefresh, selectedProds]);
 
     return (
         <Container>
+            {selectMode && userPreferences.isUserPremium && (
+                <ActionButtonsContainer>
+                    <ButtonPaper
+                        icon={() => <Icons name="trash-outline" />}
+                        onPress={handleSwitchDeleteModal}
+                    >
+                        {
+                            strings.ListProductsComponent_DeleteProducts_ActionBar_DeleteSelected
+                        }
+                    </ButtonPaper>
+
+                    <ButtonPaper
+                        icon={() => <Icons name="exit-outline" />}
+                        onPress={handleDisableSelectMode}
+                    >
+                        {
+                            strings.ListProductsComponent_DeleteProducts_ActionBar_Cancel
+                        }
+                    </ButtonPaper>
+                </ActionButtonsContainer>
+            )}
             <FlatList
                 data={products}
                 keyExtractor={item => String(item.id)}
@@ -190,6 +305,33 @@ const ListProducts: React.FC<RequestProps> = ({
                     onPress={handleNavigateAddProduct}
                 />
             )}
+
+            <Dialog.Container
+                visible={deleteModal}
+                onBackdropPress={handleSwitchDeleteModal}
+            >
+                <Dialog.Title>
+                    {strings.ListProductsComponent_DeleteProducts_Modal_Title}
+                </Dialog.Title>
+                <Dialog.Description>
+                    {
+                        strings.ListProductsComponent_DeleteProducts_Modal_Description
+                    }
+                </Dialog.Description>
+                <Dialog.Button
+                    label={
+                        strings.ListProductsComponent_DeleteProducts_Modal_Button_Keep
+                    }
+                    onPress={handleSwitchDeleteModal}
+                />
+                <Dialog.Button
+                    label={
+                        strings.ListProductsComponent_DeleteProducts_Modal_Button_Delete
+                    }
+                    color="red"
+                    onPress={handleDeleteMany}
+                />
+            </Dialog.Container>
         </Container>
     );
 };
