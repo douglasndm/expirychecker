66c80eb2664a34612d8deae89a0dfae8b990f2ab
Douglas Nunes de Mattos <douglasnunesdemattos@outlook.com>
2021-12-15 11:53:19 -0300

    Adding OneSginal Id to RevenueCat

--- src/Services/Admob.ts
+++ src/Services/Admob.ts
@@ -3,21 +3,17 @@ import { getEnableProVersion } from '../Functions/Settings';
 
 async function prepareAds() {
     if (!(await getEnableProVersion())) {
-        admob()
-            .setRequestConfiguration({
-                // Update all future requests suitable for parental guidance
-                maxAdContentRating: MaxAdContentRating.PG,
+        admob().setRequestConfiguration({
+            // Update all future requests suitable for parental guidance
+            maxAdContentRating: MaxAdContentRating.PG,
 
-                // Indicates that you want your content treated as child-directed for purposes of COPPA.
-                tagForChildDirectedTreatment: false,
+            // Indicates that you want your content treated as child-directed for purposes of COPPA.
+            tagForChildDirectedTreatment: false,
 
-                // Indicates that you want the ad request to be handled in a
-                // manner suitable for users under the age of consent.
-                tagForUnderAgeOfConsent: true,
-            })
-            .catch((err) => {
-                throw new Error(err);
-            });
+            // Indicates that you want the ad request to be handled in a
+            // manner suitable for users under the age of consent.
+            tagForUnderAgeOfConsent: true,
+        });
     }
 }
 
