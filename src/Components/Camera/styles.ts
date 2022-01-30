f5a28872f7b689c002d413433e0839d473ba1a1d
Douglas Nunes de Mattos <7841509+douglasndm@users.noreply.github.com>
2022-01-30 00:53:59 -0300

    Adding select from library pic

--- src/Components/Camera/styles.ts
+++ src/Components/Camera/styles.ts
@@ -1,37 +1,65 @@
-import styled from 'styled-components/native';
-import { Platform } from 'react-native';
+import styled, { css } from 'styled-components/native';
 import { RNCamera } from 'react-native-camera';
+import Ionicons from 'react-native-vector-icons/Ionicons';
 
 export const Container = styled.View`
     flex: 1;
-    background-color: ${(props) => props.theme.colors.background};
-    justify-content: space-between;
-    padding: ${Platform.OS === 'ios' ? 50 : 16}px 0 5px;
-`;
-
-export const PageHeader = styled.View`
-    flex-direction: row;
-    align-items: center;
-    margin-bottom: 15px;
-`;
-
-export const PageTitle = styled.Text`
-    font-size: 28px;
-    font-weight: bold;
-    color: ${(props) => props.theme.colors.text};
+    background-color: ${props => props.theme.colors.background};
 `;
 
 export const CameraContainer = styled.View`
     flex: 1;
 `;
 
+export const PreviewContainer = styled.View`
+    align-items: center;
+    justify-content: center;
+    flex: 1;
+`;
+
+export const PicPreview = styled.Image`
+    width: 85%;
+    height: 90%;
+    border-radius: 42px;
+`;
+
 export const CameraComponent = styled(RNCamera).attrs(() => ({
     captureAudio: false,
 }))`
     flex: 1;
+    background: #9159c1;
 `;
 
 export const ButtonsContainer = styled.View`
     flex-direction: row;
     justify-content: center;
+
+    margin: 5px;
+    margin-bottom: 20px;
+`;
+
+export const ShotButtonContainer = styled.View`
+    flex-direction: row;
+    justify-content: center;
+    background: ${props => props.theme.colors.accent};
+    border-radius: 12px;
+`;
+
+interface IconsProps {
+    isSelected?: boolean;
+}
+
+export const Icons = styled(Ionicons).attrs(props => ({
+    size: 50,
+    color: props.isSelected ? props.theme.colors.inputText : '#fff',
+}))<IconsProps>`
+    padding: 10px 30px;
+
+    border-top-right-radius: 12px;
+    border-bottom-right-radius: 12px;
+    ${props =>
+        props.isSelected &&
+        css`
+            background: white;
+        `}
 `;
