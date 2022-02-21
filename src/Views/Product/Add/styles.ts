45ab00502377d80a51e8f46fac326fce8283fe06
Douglas <7841509+douglasndm@users.noreply.github.com>
2022-02-21 19:38:09 -0300

    category, brand and store pickers now uses a single component

--- src/Views/Product/Add/styles.ts
+++ src/Views/Product/Add/styles.ts
@@ -113,36 +113,6 @@ export const MoreInformationsTitle = styled.Text`
     margin-bottom: 5px;
 `;
 
-export const PickerContainer = styled.View`
-    justify-content: space-between;
-
-    border: 1px solid rgba(0, 0, 0, 0.1);
-    border-radius: 12px;
-
-    background-color: ${({ theme }) => theme.colors.inputBackground};
-
-    padding: 5px;
-    font-size: 18px;
-
-    ${Platform.OS === 'ios' &&
-    css`
-        padding: 17px 10px;
-    `}
-`;
-
-export const Picker = styled(RNPickerSelect).attrs(({ theme }) => ({
-    pickerProps: {
-        style: {
-            color: theme.colors.text,
-        },
-    },
-    textInputProps: {
-        style: {
-            color: theme.colors.text,
-        },
-    },
-}))``;
-
 export const ExpDateGroup = styled.View`
     align-items: center;
 `;
