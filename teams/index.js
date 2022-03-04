32522819242aae18c83bde1d8e9931bec207c3b0
Douglas <7841509+douglasndm@users.noreply.github.com>
2022-03-04 15:10:16 -0300

    App now shows notifcations on foreground

--- teams/index.js
+++ teams/index.js
@@ -4,6 +4,7 @@
 
 import { AppRegistry, LogBox } from 'react-native';
 import messaging from '@react-native-firebase/messaging';
+import { showMessage } from 'react-native-flash-message';
 
 import { name as appName } from './app.json';
 import App from './src';
@@ -17,6 +18,16 @@ messaging().setBackgroundMessageHandler(async remoteMessage => {
         'Message handled in the background!',
         JSON.stringify(remoteMessage)
     );
+});
+
+messaging().onMessage(async remoteMessage => {
+    const { title, body } = remoteMessage.notification;
+
+    showMessage({
+        message: title,
+        description: body,
+        type: 'info',
+    });
 });
 
 AppRegistry.registerComponent(appName, () => App);
