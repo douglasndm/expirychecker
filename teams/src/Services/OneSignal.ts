82086d74cde81c93eba0b2db0536acbd6e13796e
Douglas <7841509+douglasndm@users.noreply.github.com>
2022-03-05 02:40:21 -0300

    Adding OneSignal

---
+++ teams/src/Services/OneSignal.ts
@@ -0,0 +1,33 @@
+import OneSignal from 'react-native-onesignal';
+import EnvConfig from 'react-native-config';
+
+// OneSignal Init Code
+OneSignal.setLogLevel(6, 0);
+OneSignal.setAppId(EnvConfig.ONE_SIGNAL_APP_ID);
+// END OneSignal Init Code
+
+// Prompt for push on iOS
+OneSignal.promptForPushNotificationsWithUserResponse(response => {
+    console.log('Prompt response:', response);
+});
+
+// Method for handling notifications received while app in foreground
+OneSignal.setNotificationWillShowInForegroundHandler(
+    notificationReceivedEvent => {
+        console.log(
+            'OneSignal: notification will show in foreground:',
+            notificationReceivedEvent
+        );
+        const notification = notificationReceivedEvent.getNotification();
+        console.log('notification: ', notification);
+        const data = notification.additionalData;
+        console.log('additionalData: ', data);
+        // Complete with null means don't show a notification.
+        notificationReceivedEvent.complete(notification);
+    }
+);
+
+// Method for handling notifications opened
+OneSignal.setNotificationOpenedHandler(notification => {
+    console.log('OneSignal: notification opened:', notification);
+});
