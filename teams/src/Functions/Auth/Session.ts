163ee7bf9ab8a0787cf95b72553185a993835166
Douglas Nunes de Mattos <7841509+douglasndm@users.noreply.github.com>
2022-03-14 18:35:44 -0300

    Adding user id and email to idetify on onesignal

--- teams/src/Functions/Auth/Session.ts
+++ teams/src/Functions/Auth/Session.ts
@@ -11,16 +11,25 @@ export async function createSeassion(): Promise<void> {
 
     const oneSignal = await OneSignal.getDeviceState();
     let oneSignalToken;
-
     if (oneSignal) {
         oneSignalToken = oneSignal.pushToken;
     }
 
-    await api.post(`/sessions`, {
+    let firebaseToken;
+    if (deviceId.firebase_messaging) {
+        firebaseToken = deviceId.firebase_messaging;
+    }
+
+    const response = await api.post<IUser>(`/sessions`, {
         deviceId: deviceId.device_uuid,
-        firebaseToken: deviceId.firebase_messaging,
+        firebaseToken,
         oneSignalToken,
     });
+
+    OneSignal.setEmail(response.data.email);
+    if (response.data.id) {
+        OneSignal.setExternalUserId(response.data.id);
+    }
 }
 
 export async function destroySession(): Promise<void> {
