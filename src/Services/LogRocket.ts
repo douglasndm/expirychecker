485a81dec7ff053542fab21a921ae393e81a1f24
Douglas <7841509+douglasndm@users.noreply.github.com>
2022-04-01 16:24:21 -0300

    Sync sentry with logrocket

--- src/Services/LogRocket.ts
+++ src/Services/LogRocket.ts
@@ -1,3 +1,10 @@
 import LogRocket from '@logrocket/react-native';
+import * as Sentry from '@sentry/react-native';
 
 LogRocket.init('abivwn/expiry-checker');
+
+LogRocket.getSessionURL(sessionURL => {
+    Sentry.configureScope(scope => {
+        scope.setExtra('sessionURL', sessionURL);
+    });
+});
