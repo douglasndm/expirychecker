e5f6005953b647794b7b0587d0836d4e9158e334
Douglas <7841509+douglasndm@users.noreply.github.com>
2022-04-06 21:33:20 -0300

    fix: batch view colors

--- mobile/src/Utils/System/getFormatedPrice.ts
+++ mobile/src/Utils/System/getFormatedPrice.ts
@@ -1,5 +1,17 @@
 import { getCurrentLocale } from './getLocale';
 
+function getCurrencySymbol(currency: string): string {
+    return (0)
+        .toLocaleString(getCurrentLocale(), {
+            style: 'currency',
+            currency,
+            minimumFractionDigits: 0,
+            maximumFractionDigits: 0,
+        })
+        .replace(/\d/g, '')
+        .trim();
+}
+
 function getFormatedPrice(price: number): string {
     return price.toLocaleString(getCurrentLocale(), {
         minimumFractionDigits: 2,
@@ -7,4 +19,4 @@ function getFormatedPrice(price: number): string {
     });
 }
 
-export { getFormatedPrice };
+export { getFormatedPrice, getCurrencySymbol };
