b73b4ba8f3dd6de3685bb9ebdc480f6cc8e32275
Douglas <7841509+douglasndm@users.noreply.github.com>
2022-04-11 20:25:48 -0300

    fix: categories sort on lowercase

--- mobile/src/Utils/Categories/Sort.ts
+++ mobile/src/Utils/Categories/Sort.ts
@@ -1,9 +1,12 @@
 function sortCategories(categories: ICategory[]): ICategory[] {
-    const sorted = categories.sort((b1, b2) => {
-        if (b1.name < b2.name) {
+    const sorted = categories.sort((c1, c2) => {
+        const c1name = c1.name.trim().toLowerCase();
+        const c2name = c2.name.trim().toLowerCase();
+
+        if (c1name < c2name) {
             return -1;
         }
-        if (b1.name > b2.name) {
+        if (c1name > c2name) {
             return 1;
         }
         return 0;
