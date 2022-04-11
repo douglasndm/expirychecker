e8ee6606eda4e86f4d3518bfa0cdc649340e5728
Douglas Nunes de Mattos <7841509+douglasndm@users.noreply.github.com>
2022-04-10 22:26:42 -0300

    Adding test for sort batches

--- src/Functions/Batches.ts
+++ src/Functions/Batches.ts
@@ -1,28 +1,4 @@
-import { startOfDay } from 'date-fns';
-
 import Realm from '~/Services/Realm';
-
-export function sortBatches(batches: Array<ILote>): Array<ILote> {
-    // Gambiarra para .sort da função funcionar
-    // por algum motivo sem isso sort é undefined
-    const fixed = batches.map(batch => batch);
-
-    const sorted = fixed.sort((batch1, batch2) => {
-        const date1 = startOfDay(batch1.exp_date);
-        const date2 = startOfDay(batch2.exp_date);
-
-        if (date1 > date2) {
-            return 1;
-        }
-        if (date1 < date2) {
-            return -1;
-        }
-
-        return 0;
-    });
-
-    return sorted;
-}
 
 export function removeCheckedBatches(batches: Array<ILote>): Array<ILote> {
     const filted = batches.filter(batch => batch.status !== 'checked');
