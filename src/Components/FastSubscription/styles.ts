e1bb2c078572b7647fe8e5a3a02e02ae7508e00f
Douglas <7841509+douglasndm@users.noreply.github.com>
2022-08-09 15:16:37 -0300

    fix: flex bug

--- src/Components/FastSubscription/styles.ts
+++ src/Components/FastSubscription/styles.ts
@@ -1,16 +1,14 @@
 import styled from 'styled-components/native';
 import { lighten } from 'polished';
 
-export const FastSubContainer = styled.View`
-    flex: 1;
-`;
+export const FastSubContainer = styled.View``;
 
 export const Container = styled.View`
     flex-direction: row;
     margin: 5px 10px;
     background-color: ${props =>
         lighten(0.1, props.theme.colors.inputBackground)};
-    padding: 5px 10px;
+    padding: 2px 7px;
     border-radius: 12px;
 `;
 
@@ -20,9 +18,9 @@ export const SubscriptionTextContainer = styled.View`
 `;
 
 export const SubscriptionText = styled.Text`
-    margin: 5px 10px;
     font-family: 'Open Sans';
-    font-size: 16px;
+    font-weight: bold;
+    font-size: 15px;
     text-align: center;
     color: ${props => props.theme.colors.productCardText};
 `;
