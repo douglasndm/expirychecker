0e985455bed4b01092c26809d4730ec0b0ee64cb
Douglas <7841509+douglasndm@users.noreply.github.com>
2022-08-27 16:36:43 -0300

    Moving import button to inside import/export view

--- src/Views/Settings/Components/Pro/index.tsx
+++ src/Views/Settings/Components/Pro/index.tsx
@@ -1,40 +1,22 @@
-import React, { useState, useCallback, useContext, useMemo } from 'react';
-import { View, Linking, Platform } from 'react-native';
+import React, { useCallback, useContext, useMemo } from 'react';
+import { Linking, Platform } from 'react-native';
 import { useNavigation } from '@react-navigation/native';
 import { StackNavigationProp } from '@react-navigation/stack';
-import { showMessage } from 'react-native-flash-message';
-import RNPermissions from 'react-native-permissions';
 
 import strings from '~/Locales';
 
 import PreferencesContext from '~/Contexts/PreferencesContext';
 
 import { isSubscriptionActive } from '~/Functions/ProMode';
-import { importBackupFile } from '~/Functions/Backup';
 
 import Button from '~/Components/Button';
 
-import {
-    Category,
-    CategoryTitle,
-    CategoryOptions,
-    SettingDescription,
-} from '../../styles';
+import { Category, CategoryTitle } from '../../styles';
 
-import {
-    Container,
-    PremiumButtonsContainer,
-    ButtonPremium,
-    ButtonPremiumText,
-    ButtonCancel,
-    ButtonCancelText,
-    Loading,
-} from './styles';
+import { Container, ButtonCancel, ButtonCancelText } from './styles';
 
 const Pro: React.FC = () => {
     const { userPreferences } = useContext(PreferencesContext);
-
-    const [isImportLoading, setIsImportLoading] = useState<boolean>(false);
 
     const { navigate, reset } =
         useNavigation<StackNavigationProp<RoutesParams>>();
@@ -59,46 +41,6 @@ const Pro: React.FC = () => {
         navigate('Pro');
     }, [navigate]);
 
-    const handleImportBackup = useCallback(async () => {
-        try {
-            setIsImportLoading(true);
-
-            if (Platform.OS === 'android') {
-                const isReadFileAllow = await RNPermissions.check(
-                    RNPermissions.PERMISSIONS.ANDROID.READ_EXTERNAL_STORAGE
-                );
-                if (isReadFileAllow !== 'granted') {
-                    const granted = await RNPermissions.request(
-                        RNPermissions.PERMISSIONS.ANDROID.READ_EXTERNAL_STORAGE
-                    );
-
-                    if (granted !== 'granted') {
-                        throw new Error('Permission denided');
-                    }
-                }
-            }
-
-            await importBackupFile();
-
-            showMessage({
-                message: strings.View_Settings_Backup_Import_Alert_Sucess,
-                type: 'info',
-            });
-            reset({
-                routes: [{ name: 'Home' }],
-            });
-        } catch (err) {
-            if (err instanceof Error) {
-                showMessage({
-                    message: err.message,
-                    type: 'danger',
-                });
-            }
-        } finally {
-            setIsImportLoading(false);
-        }
-    }, [reset]);
-
     return (
         <Container>
             <Category>
@@ -116,34 +58,6 @@ const Pro: React.FC = () => {
                         />
                     </>
                 )}
-
-                <CategoryOptions notPremium={!userPreferences.isPRO}>
-                    <View>
-                        <SettingDescription>
-                            {strings.View_Settings_SettingName_ExportAndInmport}
-                        </SettingDescription>
-
-                        <PremiumButtonsContainer>
-                            <ButtonPremium
-                                enabled={
-                                    userPreferences.isPRO &&
-                                    !isImportLoading
-                                }
-                                onPress={handleImportBackup}
-                            >
-                                {isImportLoading ? (
-                                    <Loading />
-                                ) : (
-                                    <ButtonPremiumText>
-                                        {
-                                            strings.View_Settings_Button_ImportFile
-                                        }
-                                    </ButtonPremiumText>
-                                )}
-                            </ButtonPremium>
-                        </PremiumButtonsContainer>
-                    </View>
-                </CategoryOptions>
 
                 {userPreferences.isPRO && (
                     <ButtonCancel onPress={handleCancel}>
