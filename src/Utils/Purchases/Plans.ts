d2a71228486eacede0f3810e2e0f3c0e92074984
Douglas <7841509+douglasndm@users.noreply.github.com>
2022-09-24 19:29:13 -0300

    Adding strings for intro days for free

--- src/Utils/Purchases/Plans.ts
+++ src/Utils/Purchases/Plans.ts
@@ -9,21 +9,40 @@ function getPlansString(pack: PurchasesPackage, per_month?: string): string {
     let text = '';
 
     if (pack.packageType === PACKAGE_TYPE.MONTHLY) {
-        if (Platform.OS === 'android') {
-            if (introPrice) {
-                text = strings.View_Subscription_Monthly_WithIntroText.replace(
-                    '{INTRO_PRICE}',
-                    introPrice.priceString
-                ).replace('{PRICE}', price_string);
-                return text;
-            }
+        if (introPrice && introPrice.periodNumberOfUnits > 0) {
+            text = strings.View_Subscription_Monthly_WithIntroTrialText;
+
+            const days = text.replace(
+                '{DAYS}',
+                String(introPrice.periodNumberOfUnits)
+            );
+            const price = days.replace('{PRICE}', price_string);
+            return price;
         }
+        if (introPrice) {
+            text = strings.View_Subscription_Monthly_WithIntroText;
+
+            const intro = text.replace('{INTRO_PRICE}', introPrice.priceString);
+            const price = intro.replace('{PRICE}', price_string);
+            return price;
+        }
+
         text = strings.View_Subscription_Monthly_Text.replace(
             '{PRICE}',
             price_string
         );
     } else if (pack.packageType === PACKAGE_TYPE.THREE_MONTH) {
         if (Platform.OS === 'android') {
+            if (introPrice && introPrice.periodNumberOfUnits > 0) {
+                text = strings.View_Subscription_3Months_WithIntroTrialText;
+
+                const days = text.replace(
+                    '{DAYS}',
+                    String(introPrice.periodNumberOfUnits)
+                );
+                const price = days.replace('{PRICE}', price_string);
+                return price;
+            }
             if (introPrice) {
                 text = strings.View_Subscription_3Months_WithIntroText.replace(
                     '{INTRO_PRICE}',
@@ -42,22 +61,25 @@ function getPlansString(pack: PurchasesPackage, per_month?: string): string {
             );
         }
     } else if (pack.packageType === PACKAGE_TYPE.ANNUAL) {
-        if (Platform.OS === 'android') {
-            if (introPrice) {
-                text = strings.View_Subscription_AYear_WithIntroText.replace(
-                    '{INTRO_PRICE}',
-                    introPrice.priceString
-                ).replace('{PRICE}', price_string);
-            } else {
-                text = strings.View_Subscription_AYear_Text.replace(
-                    '{PRICE}',
-                    price_string
-                );
-            }
+        if (introPrice && introPrice.periodNumberOfUnits > 0) {
+            text = strings.View_Subscription_AYear_WithIntroTrialText;
+
+            const days = text.replace(
+                '{DAYS}',
+                String(introPrice.periodNumberOfUnits)
+            );
+            const price = days.replace('{PRICE}', price_string);
+            return price;
+        }
+        if (introPrice) {
+            text = strings.View_Subscription_AYear_WithIntroText.replace(
+                '{INTRO_PRICE}',
+                introPrice.priceString
+            ).replace('{PRICE}', price_string);
         } else {
-            text = strings.View_Subscription_iOS_AYear_Text.replace(
+            text = strings.View_Subscription_AYear_Text.replace(
                 '{PRICE}',
-                per_month || price_string
+                price_string
             );
         }
     }
