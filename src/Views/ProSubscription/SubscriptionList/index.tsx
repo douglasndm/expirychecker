891457c9c660f4b6bd2aa8fb781051b8d9a00cae
Douglas <7841509+douglasndm@users.noreply.github.com>
2022-10-05 18:39:55 -0300

    fix: disable ads on restore pro

--- src/Views/ProSubscription/SubscriptionList/index.tsx
+++ src/Views/ProSubscription/SubscriptionList/index.tsx
@@ -132,6 +132,7 @@ const SubscriptionList: React.FC = () => {
             setUserPreferences({
                 ...userPreferences,
                 isPRO: true,
+                disableAds: true,
             });
 
             showMessage({
