52c28d1c3e88831f0c74e265c73e90883098ac4f
Douglas <7841509+douglasndm@users.noreply.github.com>
2022-11-28 20:53:45 -0300

    Adding back appsflyer

--- src/index.tsx
+++ src/index.tsx
@@ -12,13 +12,12 @@ import Analyticts from '@react-native-firebase/analytics';
 import SplashScreen from 'react-native-splash-screen';
 import FlashMessage from 'react-native-flash-message';
 import { enableScreens } from 'react-native-screens';
-import { Adjust } from 'react-native-adjust';
 
 import './Locales';
 
 import './Services/DeviceId';
 import './Services/BackgroundJobs';
-import './Services/Adjust';
+import './Services/AppsFlyer';
 import './Services/Admob';
 import './Services/Analytics';
 import './Services/RemoteConfig';
@@ -90,10 +89,6 @@ const App: React.FC = () => {
     useEffect(() => {
         loadInitialData();
     }, [loadInitialData]);
-
-    useEffect(() => {
-        return () => Adjust.componentWillUnmount();
-    }, []);
 
     return (
         <PreferencesContext.Provider
