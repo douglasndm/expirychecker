794bb97c32db362532d4ff408b593b65f8bbfe71
Douglas <7841509+douglasndm@users.noreply.github.com>
2022-12-18 13:52:46 -0300

    Adding local implementation of header

--- src/Components/Header/index.tsx
+++ src/Components/Header/index.tsx
@@ -1,3 +1,27 @@
-import Header from '@components/Header';
+import React, { useContext, useMemo } from 'react';
 
-export default Header;
+import strings from '~/Locales';
+
+import PreferencesContext from '~/Contexts/PreferencesContext';
+
+import Header, { RequestProps} from '@components/Header';
+
+const LocalHeader: React.FC<RequestProps> = (props) => {
+    const { userPreferences } = useContext(PreferencesContext);
+
+    const title = useMemo(() => {
+        if(props.title) {
+            return props.title;
+        }
+
+        if(!props.title && userPreferences.isPRO) {
+            return strings.AppName_ProVersion;
+        }
+
+        return strings.AppName;
+    }, [])
+
+    return <Header {...props} title={title} />
+}
+
+export default LocalHeader;
