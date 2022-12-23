366daec4f80a8f7ab5bec0f5ff4ca7c1c66394ac
Douglas Nunes de Mattos <7841509+douglasndm@users.noreply.github.com>
2022-12-23 17:30:45 -0300

    Setting days to be next is now shared

--- src/Contexts/PreferencesContext.ts
+++ src/Contexts/PreferencesContext.ts
@@ -1,32 +1,29 @@
 import { createContext } from 'react';
-import { IUserPreferences } from '~/@types/userPreference';
-
-import { NotificationCadency } from '../Functions/Settings';
 import Themes from '@shared/Themes';
 
+import { IUserPreferences } from '@expirychecker/@types/userPreference';
+
 const PreferencesContext = createContext({
-    userPreferences: {
-        howManyDaysToBeNextToExpire: 30,
-        autoComplete: false,
-        isPRO: false,
-        appTheme: Themes.Light,
-        multiplesStores: false,
-        storesFirstPage: false,
-        enableNotifications: true,
-        notificationCadency: NotificationCadency.Day,
-        disableAds: false,
-    },
-    setUserPreferences: ({
-        howManyDaysToBeNextToExpire,
-        autoComplete,
-        isPRO,
-        appTheme,
-        multiplesStores,
-        storesFirstPage,
-        enableNotifications,
-        notificationCadency,
-        disableAds,
-    }: IUserPreferences) => {},
+	userPreferences: {
+		howManyDaysToBeNextToExpire: 30,
+		autoComplete: false,
+		isPRO: false,
+		appTheme: Themes.Light,
+		multiplesStores: false,
+		storesFirstPage: false,
+		enableNotifications: true,
+		disableAds: false,
+	},
+	setUserPreferences: ({
+		howManyDaysToBeNextToExpire,
+		autoComplete,
+		isPRO,
+		appTheme,
+		multiplesStores,
+		storesFirstPage,
+		enableNotifications,
+		disableAds,
+	}: IUserPreferences) => {},
 });
 
 export default PreferencesContext;
--- src/Functions/UserPreferences.ts
+++ src/Functions/UserPreferences.ts
@@ -1,40 +1,37 @@
-import { IUserPreferences } from '../@types/userPreference';
 import { getThemeByName } from '@shared/Themes';
+import { IUserPreferences } from '../@types/userPreference';
 
 import {
-    getEnableMultipleStoresMode,
-    getEnableNotifications,
-    getNotificationCadency,
-    getEnableProVersion,
-    getHowManyDaysToBeNextExp,
-    getDisableAds,
-    getStoreFirstPage,
-    getAutoComplete,
+	getEnableMultipleStoresMode,
+	getEnableNotifications,
+	getEnableProVersion,
+	getHowManyDaysToBeNextExp,
+	getDisableAds,
+	getStoreFirstPage,
+	getAutoComplete,
 } from './Settings';
 import { getAppTheme } from './Themes';
 
 export async function getAllUserPreferences(): Promise<IUserPreferences> {
-    const settingDay = await getHowManyDaysToBeNextExp();
-    const settingAutoComplete = await getAutoComplete();
-    const settingTheme = await getAppTheme();
-    const settingNotification = await getEnableNotifications();
-    const settingMultipleStores = await getEnableMultipleStoresMode();
-    const settingStoresFirstPage = await getStoreFirstPage();
-    const settingNotificationCadency = await getNotificationCadency();
-    const settingProMode = await getEnableProVersion();
-    const disableAds = await getDisableAds();
+	const settingDay = await getHowManyDaysToBeNextExp();
+	const settingAutoComplete = await getAutoComplete();
+	const settingTheme = await getAppTheme();
+	const settingNotification = await getEnableNotifications();
+	const settingMultipleStores = await getEnableMultipleStoresMode();
+	const settingStoresFirstPage = await getStoreFirstPage();
+	const settingProMode = await getEnableProVersion();
+	const disableAds = await getDisableAds();
 
-    const settings: IUserPreferences = {
-        howManyDaysToBeNextToExpire: settingDay,
-        autoComplete: settingAutoComplete,
-        appTheme: getThemeByName(settingTheme),
-        enableNotifications: settingNotification,
-        notificationCadency: settingNotificationCadency,
-        isPRO: settingProMode,
-        multiplesStores: settingMultipleStores,
-        storesFirstPage: settingStoresFirstPage,
-        disableAds: disableAds || settingProMode,
-    };
+	const settings: IUserPreferences = {
+		howManyDaysToBeNextToExpire: settingDay,
+		autoComplete: settingAutoComplete,
+		appTheme: getThemeByName(settingTheme),
+		enableNotifications: settingNotification,
+		isPRO: settingProMode,
+		multiplesStores: settingMultipleStores,
+		storesFirstPage: settingStoresFirstPage,
+		disableAds: disableAds || settingProMode,
+	};
 
-    return settings;
+	return settings;
 }
