7786e3560bf8f2ee8690db88ce0131ba3c782041
Douglas <7841509+douglasndm@users.noreply.github.com>
2022-12-28 19:11:21 -0300

    Moving WeekView to shared component

--- src/Views/Product/WeekView/index.tsx
+++ src/Views/Product/WeekView/index.tsx
@@ -1,127 +1,32 @@
-import React, {
-	useState,
-	useEffect,
-	useCallback,
-	useMemo,
-	useContext,
-} from 'react';
-import {
-	addDays,
-	compareAsc,
-	eachWeekOfInterval,
-	format,
-	isPast,
-} from 'date-fns';
-import { getLocales } from 'react-native-localize';
+import React, { useState, useEffect, useCallback, useContext } from 'react';
 import { showMessage } from 'react-native-flash-message';
-import Accordion from 'react-native-collapsible/Accordion';
-
-import strings from '@expirychecker/Locales';
 
 import PreferencesContext from '@expirychecker/Contexts/PreferencesContext';
 
-import Loading from '@components/Loading';
-import Header from '@components/Header';
-
-import ProductCard from '@expirychecker/Components/ListProducts/ProductCard';
-import FloatButton from '@expirychecker/Components/FloatButton';
-
 import { getAllProducts } from '@expirychecker/Functions/Products';
 
-import {
-	Container,
-	PageContent,
-	ProductCount,
-	WeekContainer,
-	WeekText,
-} from './styles';
+import WeekViewComponent from '@views/Product/WeekView';
 
 const WeekView: React.FC = () => {
 	const { userPreferences } = useContext(PreferencesContext);
 
-	const [isLoading, setIsLoading] = useState<boolean>(true);
-
-	const [weeks, setWeeks] = useState<WeekProps[]>([]);
-	const [activeSection, setActiveSection] = useState<number[]>([]);
-
-	const dateFormat = useMemo(() => {
-		if (getLocales()[0].languageCode === 'en') {
-			return 'MM/dd/yyyy';
-		}
-		return 'dd/MM/yyyy';
-	}, []);
+	const [products, setProducts] = useState<IProduct[]>([]);
 
 	const loadData = useCallback(async () => {
 		try {
-			setIsLoading(true);
-
 			const allProducts = await getAllProducts({
 				sortProductsByExpDate: true,
 				removeTreatedBatch: true,
 			});
 
-			const batches: ILote[] = [];
-
-			allProducts.forEach(p => p.batches.forEach(b => batches.push(b)));
-
-			const sorted = batches.sort((b1, b2) =>
-				compareAsc(b1.exp_date, b2.exp_date)
-			);
-
-			if (sorted.length > 0) {
-				const weeksInUse = eachWeekOfInterval({
-					start: sorted[0].exp_date,
-					end: sorted[sorted.length - 1].exp_date,
-				});
-
-				const weeksProds = weeksInUse.map(week => {
-					const prods: IProduct[] = [];
-
-					const weekLimit = addDays(week, 7);
-
-					allProducts.forEach(prod => {
-						const filtedBatches = prod.batches.filter(b => {
-							const { exp_date } = b;
-
-							if (compareAsc(weekLimit, exp_date) >= 0) {
-								if (compareAsc(exp_date, week) >= 0) {
-									if (b.status?.toLowerCase() !== 'tratado') {
-										return true;
-									}
-								}
-							}
-							return false;
-						});
-
-						if (filtedBatches.length > 0) {
-							prods.push({
-								...prod,
-								batches: filtedBatches,
-							});
-						}
-					});
-
-					const weekProps: WeekProps = {
-						date: week,
-						products: prods,
-					};
-					return weekProps;
-				});
-
-				const usedWeeks = weeksProds.filter(
-					wp => wp.products.length > 0
-				);
-
-				setWeeks(usedWeeks);
-			}
+			setProducts(allProducts);
 		} catch (err) {
-			if (err instanceof Error)
+			if (err instanceof Error) {
 				showMessage({
-					message: err.message,
 					type: 'danger',
+					message: err.message,
 				});
-		} finally {
-			setIsLoading(false);
+			}
 		}
 	}, []);
 
@@ -129,78 +34,13 @@ const WeekView: React.FC = () => {
 		loadData();
 	}, []);
 
-	const renderSectionTitle = useCallback(
-		(week: WeekProps, index: number) => {
-			const onPress = () => {
-				if (activeSection[0] === index) {
-					setActiveSection([]);
-					return;
-				}
-				setActiveSection([index]);
-			};
-
-			const dateFormatted = format(week.date, dateFormat);
-			const isExpired = isPast(week.date);
-			const isNext =
-				addDays(
-					new Date(),
-					userPreferences.howManyDaysToBeNextToExpire
-				) >= week.date;
-
-			return (
-				<WeekContainer
-					onPress={onPress}
-					isPast={isExpired}
-					isNext={isNext}
-				>
-					<WeekText isPast={isExpired} isNext={isNext}>
-						{`${strings.View_ProductsByWeeks_WeekTitle} ${dateFormatted}`}
-					</WeekText>
-					<ProductCount isPast={isExpired} isNext={isNext}>
-						{`${strings.View_ProductsByWeeks_ProductsCount.replace(
-							'{COUNT}',
-							String(week.products.length)
-						)}`}
-					</ProductCount>
-				</WeekContainer>
-			);
-		},
-		[activeSection, dateFormat, userPreferences.howManyDaysToBeNextToExpire]
-	);
-
-	const renderContent = useCallback((week: WeekProps) => {
-		return week.products.map(prod => (
-			<ProductCard key={prod.id} product={prod} />
-		));
-	}, []);
-
-	const sections = useMemo(() => {
-		return weeks.map(week => {
-			return {
-				date: week.date,
-				products: week.products,
-			};
-		});
-	}, [weeks]);
-
-	return isLoading ? (
-		<Loading />
-	) : (
-		<Container>
-			<Header title={strings.View_ProductsByWeeks_Title} />
-
-			<PageContent>
-				<Accordion
-					sections={sections}
-					activeSections={activeSection}
-					renderHeader={() => <></>}
-					renderSectionTitle={renderSectionTitle}
-					renderContent={renderContent}
-				/>
-			</PageContent>
-
-			<FloatButton navigateTo="AddProduct" />
-		</Container>
+	return (
+		<WeekViewComponent
+			products={products}
+			howManyDaysToBeNextToExpire={
+				userPreferences.howManyDaysToBeNextToExpire
+			}
+		/>
 	);
 };
 
--- src/Views/Success/index.tsx
+++ src/Views/Success/index.tsx
@@ -1,277 +1,275 @@
 import React, { useCallback, useContext, useMemo } from 'react';
 import {
-    useNavigation,
-    useRoute,
-    StackActions,
+	useNavigation,
+	useRoute,
+	StackActions,
 } from '@react-navigation/native';
 import { StackNavigationProp } from '@react-navigation/stack';
 import LottieView from 'lottie-react-native';
 import { BannerAdSize } from 'react-native-google-mobile-ads';
 
-import strings from '~/Locales';
-
-import PreferencesContext from '~/Contexts/PreferencesContext';
-
 import StatusBar from '@components/StatusBar';
 import BackButton from '@components/BackButton';
-import FloatButton from '~/Components/FloatButton';
-import Banner from '~/Components/Ads/Banner';
+import FloatButton from '@components/FloatButton';
 import PaddingComponent from '@components/PaddingComponent';
+import PreferencesContext from '~/Contexts/PreferencesContext';
+import Banner from '~/Components/Ads/Banner';
+import strings from '~/Locales';
 
 import {
-    Container,
-    Content,
-    SuccessMessageContainer,
-    Title,
-    Description,
-    ButtonContainer,
-    Button,
-    ButtonText,
+	Container,
+	Content,
+	SuccessMessageContainer,
+	Title,
+	Description,
+	ButtonContainer,
+	Button,
+	ButtonText,
 } from './styles';
 
 interface Props {
-    type:
-        | 'create_batch'
-        | 'create_product'
-        | 'edit_batch'
-        | 'edit_product'
-        | 'delete_batch'
-        | 'delete_product';
-    productId?: number;
+	type:
+		| 'create_batch'
+		| 'create_product'
+		| 'edit_batch'
+		| 'edit_product'
+		| 'delete_batch'
+		| 'delete_product';
+	productId?: number;
 
-    category_id?: string;
-    store_id?: string;
+	category_id?: string;
+	store_id?: string;
 }
 
 const Success: React.FC = () => {
-    const route = useRoute();
-    const { reset, replace, dispatch } =
-        useNavigation<StackNavigationProp<RoutesParams>>();
+	const route = useRoute();
+	const { reset, replace, dispatch } =
+		useNavigation<StackNavigationProp<RoutesParams>>();
 
-    const { userPreferences } = useContext(PreferencesContext);
+	const { userPreferences } = useContext(PreferencesContext);
 
-    const routeParams = route.params as Props;
+	const routeParams = route.params as Props;
 
-    const type = useMemo(() => {
-        return routeParams.type;
-    }, [routeParams]);
+	const type = useMemo(() => {
+		return routeParams.type;
+	}, [routeParams]);
 
-    const productId = useMemo(() => {
-        if (routeParams.productId) {
-            return routeParams.productId;
-        }
-        return undefined;
-    }, [routeParams.productId]);
+	const productId = useMemo(() => {
+		if (routeParams.productId) {
+			return routeParams.productId;
+		}
+		return undefined;
+	}, [routeParams.productId]);
 
-    const category_id = useMemo(() => {
-        if (!routeParams.category_id || routeParams.category_id === 'null') {
-            return null;
-        }
-        return routeParams.category_id;
-    }, [routeParams.category_id]);
+	const category_id = useMemo(() => {
+		if (!routeParams.category_id || routeParams.category_id === 'null') {
+			return null;
+		}
+		return routeParams.category_id;
+	}, [routeParams.category_id]);
 
-    const store_id = useMemo(() => {
-        if (!routeParams.store_id || routeParams.store_id === 'null') {
-            return null;
-        }
-        return routeParams.store_id;
-    }, [routeParams.store_id]);
+	const store_id = useMemo(() => {
+		if (!routeParams.store_id || routeParams.store_id === 'null') {
+			return null;
+		}
+		return routeParams.store_id;
+	}, [routeParams.store_id]);
 
-    const handleNavigateToCategory = useCallback(() => {
-        reset({
-            routes: [
-                {
-                    name: 'Home',
-                },
-                {
-                    name: 'CategoryView',
-                    params: {
-                        id: category_id,
-                    },
-                },
-            ],
-        });
-    }, [category_id, reset]);
+	const handleNavigateToCategory = useCallback(() => {
+		reset({
+			routes: [
+				{
+					name: 'Home',
+				},
+				{
+					name: 'CategoryView',
+					params: {
+						id: category_id,
+					},
+				},
+			],
+		});
+	}, [category_id, reset]);
 
-    const handleNavigateToStore = useCallback(() => {
-        reset({
-            routes: [
-                {
-                    name: 'Home',
-                },
-                {
-                    name: 'StoreDetails',
-                    params: {
-                        store: store_id,
-                    },
-                },
-            ],
-        });
-    }, [store_id, reset]);
+	const handleNavigateToStore = useCallback(() => {
+		reset({
+			routes: [
+				{
+					name: 'Home',
+				},
+				{
+					name: 'StoreDetails',
+					params: {
+						store: store_id,
+					},
+				},
+			],
+		});
+	}, [store_id, reset]);
 
-    const animation = useMemo(() => {
-        switch (type) {
-            case 'delete_batch':
-                return require('../../Assets/Animations/delete-animation.json');
-            case 'delete_product':
-                return require('../../Assets/Animations/delete-animation.json');
-            default:
-                return require('../../Assets/Animations/success.json');
-        }
-    }, [type]);
+	const animation = useMemo(() => {
+		switch (type) {
+			case 'delete_batch':
+				return require('../../Assets/Animations/delete-animation.json');
+			case 'delete_product':
+				return require('../../Assets/Animations/delete-animation.json');
+			default:
+				return require('../../Assets/Animations/success.json');
+		}
+	}, [type]);
 
-    const handleNavigateHome = useCallback(() => {
-        reset({
-            routes: [{ name: 'Home' }],
-        });
-    }, [reset]);
+	const handleNavigateHome = useCallback(() => {
+		reset({
+			routes: [{ name: 'Home' }],
+		});
+	}, [reset]);
 
-    const handleNavigateToProduct = useCallback(() => {
-        if (routeParams.productId) {
-            if (
-                type === 'create_batch' ||
-                type === 'create_product' ||
-                type === 'edit_batch' ||
-                type === 'edit_product'
-            ) {
-                replace('ProductDetails', {
-                    id: routeParams.productId,
-                });
-            }
-        }
-    }, [replace, routeParams.productId, type]);
+	const handleNavigateToProduct = useCallback(() => {
+		if (routeParams.productId) {
+			if (
+				type === 'create_batch' ||
+				type === 'create_product' ||
+				type === 'edit_batch' ||
+				type === 'edit_product'
+			) {
+				replace('ProductDetails', {
+					id: routeParams.productId,
+				});
+			}
+		}
+	}, [replace, routeParams.productId, type]);
 
-    const handleGoBack = useCallback(() => {
-        let popAction;
+	const handleGoBack = useCallback(() => {
+		let popAction;
 
-        if (type === 'delete_product') {
-            popAction = StackActions.pop(3);
-        } else {
-            popAction = StackActions.pop(2);
-        }
-        dispatch(popAction);
-    }, [dispatch, type]);
+		if (type === 'delete_product') {
+			popAction = StackActions.pop(3);
+		} else {
+			popAction = StackActions.pop(2);
+		}
+		dispatch(popAction);
+	}, [dispatch, type]);
 
-    return (
-        <Container>
-            <StatusBar />
+	return (
+		<Container>
+			<StatusBar />
 
-            <BackButton handleOnPress={handleGoBack} />
+			<BackButton handleOnPress={handleGoBack} />
 
-            <Content>
-                <SuccessMessageContainer>
-                    <LottieView
-                        source={animation}
-                        autoPlay
-                        loop={false}
-                        style={{ width: 130, height: 130 }}
-                    />
+			<Content>
+				<SuccessMessageContainer>
+					<LottieView
+						source={animation}
+						autoPlay
+						loop={false}
+						style={{ width: 130, height: 130 }}
+					/>
 
-                    {type === 'create_batch' && (
-                        <Title>{strings.View_Success_BatchCreated}</Title>
-                    )}
-                    {type === 'create_product' && (
-                        <Title>{strings.View_Success_ProductCreated}</Title>
-                    )}
-                    {type === 'edit_batch' && (
-                        <Title>{strings.View_Success_BatchUpdated}</Title>
-                    )}
-                    {type === 'edit_product' && (
-                        <Title>{strings.View_Success_ProductUpdated}</Title>
-                    )}
-                    {type === 'delete_batch' && (
-                        <Title>{strings.View_Success_BatchDeleted}</Title>
-                    )}
-                    {type === 'delete_product' && (
-                        <Title>{strings.View_Success_ProductDeleted}</Title>
-                    )}
+					{type === 'create_batch' && (
+						<Title>{strings.View_Success_BatchCreated}</Title>
+					)}
+					{type === 'create_product' && (
+						<Title>{strings.View_Success_ProductCreated}</Title>
+					)}
+					{type === 'edit_batch' && (
+						<Title>{strings.View_Success_BatchUpdated}</Title>
+					)}
+					{type === 'edit_product' && (
+						<Title>{strings.View_Success_ProductUpdated}</Title>
+					)}
+					{type === 'delete_batch' && (
+						<Title>{strings.View_Success_BatchDeleted}</Title>
+					)}
+					{type === 'delete_product' && (
+						<Title>{strings.View_Success_ProductDeleted}</Title>
+					)}
 
-                    {type === 'create_batch' && (
-                        <Description>
-                            {strings.View_Success_BatchCreatedDescription}
-                        </Description>
-                    )}
-                    {type === 'create_product' && (
-                        <Description>
-                            {strings.View_Success_ProductCreatedDescription}
-                        </Description>
-                    )}
-                    {type === 'edit_batch' && (
-                        <Description>
-                            {strings.View_Success_BatchUpdatedDescription}
-                        </Description>
-                    )}
-                    {type === 'edit_product' && (
-                        <Description>
-                            {strings.View_Success_ProductUpdatedDescription}
-                        </Description>
-                    )}
-                    {type === 'delete_batch' && (
-                        <Description>
-                            {strings.View_Success_BatchDeletedDescription}
-                        </Description>
-                    )}
-                    {type === 'delete_product' && (
-                        <Description>
-                            {strings.View_Success_ProductDeletedDescription}
-                        </Description>
-                    )}
+					{type === 'create_batch' && (
+						<Description>
+							{strings.View_Success_BatchCreatedDescription}
+						</Description>
+					)}
+					{type === 'create_product' && (
+						<Description>
+							{strings.View_Success_ProductCreatedDescription}
+						</Description>
+					)}
+					{type === 'edit_batch' && (
+						<Description>
+							{strings.View_Success_BatchUpdatedDescription}
+						</Description>
+					)}
+					{type === 'edit_product' && (
+						<Description>
+							{strings.View_Success_ProductUpdatedDescription}
+						</Description>
+					)}
+					{type === 'delete_batch' && (
+						<Description>
+							{strings.View_Success_BatchDeletedDescription}
+						</Description>
+					)}
+					{type === 'delete_product' && (
+						<Description>
+							{strings.View_Success_ProductDeletedDescription}
+						</Description>
+					)}
 
-                    <Banner
-                        adFor="Success"
-                        size={BannerAdSize.MEDIUM_RECTANGLE}
-                    />
+					<Banner
+						adFor="Success"
+						size={BannerAdSize.MEDIUM_RECTANGLE}
+					/>
 
-                    <ButtonContainer>
-                        {userPreferences.isPRO && category_id && (
-                            <Button onPress={handleNavigateToCategory}>
-                                <ButtonText>
-                                    {strings.View_Success_Button_GoToCategory}
-                                </ButtonText>
-                            </Button>
-                        )}
+					<ButtonContainer>
+						{userPreferences.isPRO && category_id && (
+							<Button onPress={handleNavigateToCategory}>
+								<ButtonText>
+									{strings.View_Success_Button_GoToCategory}
+								</ButtonText>
+							</Button>
+						)}
 
-                        {userPreferences.isPRO && store_id && (
-                            <Button onPress={handleNavigateToStore}>
-                                <ButtonText>
-                                    {strings.View_Success_Button_GoToStore}
-                                </ButtonText>
-                            </Button>
-                        )}
+						{userPreferences.isPRO && store_id && (
+							<Button onPress={handleNavigateToStore}>
+								<ButtonText>
+									{strings.View_Success_Button_GoToStore}
+								</ButtonText>
+							</Button>
+						)}
 
-                        <Button onPress={handleNavigateHome}>
-                            <ButtonText>
-                                {strings.View_Success_Button_GoToHome}
-                            </ButtonText>
-                        </Button>
+						<Button onPress={handleNavigateHome}>
+							<ButtonText>
+								{strings.View_Success_Button_GoToHome}
+							</ButtonText>
+						</Button>
 
-                        {(type === 'create_batch' ||
-                            type === 'create_product' ||
-                            type === 'edit_batch' ||
-                            type === 'edit_product') &&
-                            !!routeParams.productId && (
-                                <Button onPress={handleNavigateToProduct}>
-                                    <ButtonText>
-                                        {
-                                            strings.View_Success_Button_NavigateToProduct
-                                        }
-                                    </ButtonText>
-                                </Button>
-                            )}
-                    </ButtonContainer>
-                </SuccessMessageContainer>
+						{(type === 'create_batch' ||
+							type === 'create_product' ||
+							type === 'edit_batch' ||
+							type === 'edit_product') &&
+							!!routeParams.productId && (
+								<Button onPress={handleNavigateToProduct}>
+									<ButtonText>
+										{
+											strings.View_Success_Button_NavigateToProduct
+										}
+									</ButtonText>
+								</Button>
+							)}
+					</ButtonContainer>
+				</SuccessMessageContainer>
 
-                <PaddingComponent />
-            </Content>
+				<PaddingComponent />
+			</Content>
 
-            {type === 'create_product' && userPreferences.isPRO && (
-                <FloatButton navigateTo="AddProduct" />
-            )}
-            {type === 'create_batch' && userPreferences.isPRO && productId && (
-                <FloatButton navigateTo="AddBatch" productId={productId} />
-            )}
-        </Container>
-    );
+			{type === 'create_product' && userPreferences.isPRO && (
+				<FloatButton navigateTo="AddProduct" />
+			)}
+			{type === 'create_batch' && userPreferences.isPRO && productId && (
+				<FloatButton navigateTo="AddBatch" productId={productId} />
+			)}
+		</Container>
+	);
 };
 
 export default Success;
