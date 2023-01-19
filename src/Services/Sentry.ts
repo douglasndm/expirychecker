36f6007e69ca459a017b9dfe15b3ecc1fa9a4298
Douglas <7841509+douglasndm@users.noreply.github.com>
2023-01-18 22:27:05 -0300

    Upgradting no RN 0.71

--- src/Services/Sentry.ts
+++ src/Services/Sentry.ts
@@ -1,12 +1,13 @@
 import * as Sentry from '@sentry/react-native';
 import EnvConfig from 'react-native-config';
 
-Sentry.init({
-	dsn: EnvConfig.SENTRY_DSN,
-	// Set tracesSampleRate to 1.0 to capture 100% of transactions for performance monitoring.
-	// We recommend adjusting this value in production.
-	tracesSampleRate: 0.5,
-	enabled: !__DEV__,
-});
+if (!__DEV__)
+	Sentry.init({
+		dsn: EnvConfig.SENTRY_DSN,
+		// Set tracesSampleRate to 1.0 to capture 100% of transactions for performance monitoring.
+		// We recommend adjusting this value in production.
+		tracesSampleRate: 0.5,
+		enabled: !__DEV__,
+	});
 
 export default Sentry;
