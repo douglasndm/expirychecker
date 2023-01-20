ba68d39d53eedec33b12eca2e0bebfc57470431d
Douglas <7841509+douglasndm@users.noreply.github.com>
2023-01-20 17:45:25 -0300

    Updating fast sub button

--- src/Components/FastSubscription/index.tsx
+++ src/Components/FastSubscription/index.tsx
@@ -10,20 +10,23 @@ import { PACKAGE_TYPE, PurchasesPackage } from 'react-native-purchases';
 import Analytics from '@react-native-firebase/analytics';
 import { showMessage } from 'react-native-flash-message';
 
+import strings from '@expirychecker/Locales';
+
+import PreferencesContext from '@expirychecker/Contexts/PreferencesContext';
+
+import { getEnableProVersion } from '@expirychecker/Functions/Settings';
+import {
+	getSubscriptionDetails,
+	makeSubscription,
+} from '@expirychecker/Functions/ProMode';
+
 import Loading from '@components/Loading';
 import Button from '@components/Button';
-
-import strings from '~/Locales';
-
-import PreferencesContext from '~/Contexts/PreferencesContext';
-
-import { getEnableProVersion } from '~/Functions/Settings';
-import { getSubscriptionDetails, makeSubscription } from '~/Functions/ProMode';
 
 import {
 	TermsPrivacyLink,
 	TermsPrivacyText,
-} from '~/Views/ProSubscription/styles';
+} from '@expirychecker/Views/ProSubscription/styles';
 
 import {
 	FastSubContainer,
@@ -46,23 +49,34 @@ const FastSubscription: React.FC = () => {
 	const { userPreferences, setUserPreferences } =
 		useContext(PreferencesContext);
 
-	const [monthlyPlan, setMonthlyPlan] = useState<
-		PurchasesPackage | undefined
-	>();
+	const [plan, setPlan] = useState<PurchasesPackage | undefined>();
 
 	const monthlyString = useMemo(() => {
 		let string = '';
 
-		if (monthlyPlan) {
-			const { priceString } = monthlyPlan.product;
+		if (plan) {
+			const { priceString, introPrice, price } = plan.product;
 
-			string = strings.Component_FastSub_Price.replace(
-				'{PRICE}',
-				priceString
-			);
+			if (introPrice) {
+				if (introPrice?.periodNumberOfUnits > 0) {
+					const packMonthlyPrice = price / 12;
+
+					string = strings.Component_FastSub_TextWithTrial.replace(
+						'{DAYS}',
+						String(introPrice?.periodNumberOfUnits)
+					)
+						.replace('{PRICE_MONTH}', packMonthlyPrice.toFixed(2))
+						.replace('{PRICE}', priceString);
+				}
+			} else {
+				string = strings.Component_FastSub_Text.replace(
+					'{PRICE}',
+					priceString
+				);
+			}
 		}
 		return string;
-	}, [monthlyPlan]);
+	}, [plan]);
 
 	const loadData = useCallback(async () => {
 		try {
@@ -71,8 +85,9 @@ const FastSubscription: React.FC = () => {
 			const response = await getSubscriptionDetails();
 
 			response.forEach(packageItem => {
-				if (packageItem.packageType === PACKAGE_TYPE.MONTHLY) {
-					setMonthlyPlan(packageItem);
+				if (packageItem.packageType === PACKAGE_TYPE.ANNUAL) {
+					console.log(packageItem);
+					setPlan(packageItem);
 				}
 			});
 		} finally {
@@ -88,11 +103,11 @@ const FastSubscription: React.FC = () => {
 		try {
 			setIsLoading(true);
 
-			if (!monthlyPlan) {
+			if (!plan) {
 				return;
 			}
 
-			await makeSubscription(monthlyPlan);
+			await makeSubscription(plan);
 
 			const enablePro = await getEnableProVersion();
 
@@ -116,20 +131,16 @@ const FastSubscription: React.FC = () => {
 		} finally {
 			setIsLoading(false);
 		}
-	}, [monthlyPlan, setUserPreferences, userPreferences]);
+	}, [plan, setUserPreferences, userPreferences]);
 
 	return isLoading ? (
 		<Loading disableText />
 	) : (
 		<>
-			{monthlyPlan && (
+			{plan && (
 				<FastSubContainer>
 					<Container>
 						<SubscriptionTextContainer>
-							<SubscriptionText>
-								{strings.Component_FastSub_Text}
-							</SubscriptionText>
-
 							<SubscriptionText>{monthlyString}</SubscriptionText>
 						</SubscriptionTextContainer>
 
@@ -137,8 +148,13 @@ const FastSubscription: React.FC = () => {
 							text={strings.View_ProPage_Button_Subscribe}
 							onPress={subscribe}
 							isLoading={isLoading}
+							contentStyle={{ backgroundColor: '#ffffff' }}
+							textStyle={{ color: '#000000' }}
 						/>
 					</Container>
+					<TermsPrivacyText>
+						{strings.View_Subscription_Disclaim_IntroPrice}
+					</TermsPrivacyText>
 					<TermsPrivacyText>
 						{strings.View_ProPage_Text_BeforeTermsAndPrivacy}
 						<TermsPrivacyLink onPress={navigateToTerms}>
--- src/Components/FastSubscription/styles.ts
+++ src/Components/FastSubscription/styles.ts
@@ -1,26 +1,25 @@
 import styled from 'styled-components/native';
-import { lighten } from 'polished';
 
 export const FastSubContainer = styled.View``;
 
 export const Container = styled.View`
-    flex-direction: row;
-    margin: 5px 10px;
-    background-color: ${props =>
-        lighten(0.1, props.theme.colors.inputBackground)};
-    padding: 2px 7px;
-    border-radius: 12px;
+	flex-direction: row;
+	margin: 10px;
+	background-color: #e8254f
+	padding: 25px 5px;
+	border-radius: 8px;
 `;
 
 export const SubscriptionTextContainer = styled.View`
-    flex: 1;
-    justify-content: center;
+	flex: 1;
+	justify-content: center;
+	margin-right: 10px;
 `;
 
 export const SubscriptionText = styled.Text`
-    font-family: 'Open Sans';
-    font-weight: bold;
-    font-size: 15px;
-    text-align: center;
-    color: ${props => props.theme.colors.productCardText};
+	font-family: 'Open Sans';
+	font-weight: bold;
+	font-size: 15px;
+	text-align: center;
+	color: ${props => props.theme.colors.productCardText};
 `;
