2e9d62984e7faf54e88a87dc99af14dca74c20e9
Douglas <7841509+douglasndm@users.noreply.github.com>
2023-01-30 17:03:20 -0300

    Adding codepush dist on sentry

--- src/Services/Sentry.ts
+++ src/Services/Sentry.ts
@@ -1,14 +1,27 @@
 import * as Sentry from '@sentry/react-native';
+import codePush from 'react-native-code-push';
 import EnvConfig from 'react-native-config';
 
-if (!__DEV__)
-	Sentry.init({
+if (!__DEV__) {
+	const configs: Sentry.ReactNativeOptions = {
 		dsn: EnvConfig.SENTRY_DSN,
 		// Set tracesSampleRate to 1.0 to capture 100% of transactions for performance monitoring.
 		// We recommend adjusting this value in production.
 		tracesSampleRate: 0.5,
 		enabled: !__DEV__,
 		attachScreenshot: true,
+	};
+	codePush.getUpdateMetadata().then(update => {
+		if (update) {
+			Sentry.init({
+				...configs,
+				release: `${update.appVersion}+codepush:${update.label}`,
+				dist: update.label,
+			});
+		} else {
+			Sentry.init(configs);
+		}
 	});
+}
 
 export default Sentry;
