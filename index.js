d6519e155d5f0b6927f669278aab7ac0b2eb7029
Douglas Nunes de Mattos <7841509+douglasndm@users.noreply.github.com>
2023-01-30 12:05:48 -0300

    Moving deep linking handler to shared project

--- index.js
+++ index.js
@@ -2,46 +2,21 @@
  * @format
  */
 
-import { AppRegistry, Linking } from 'react-native';
+import { AppRegistry } from 'react-native';
 import messaging from '@react-native-firebase/messaging';
-import { showMessage } from 'react-native-flash-message';
 import Sentry from '@expirychecker/Services/Sentry';
 
-import '@shared/Services/Notifications';
+import '@services/Notifications';
+import '@services/PushNotificationHandler';
 import { handleSetNotification } from '@expirychecker/Services/BackgroundJobs';
 
 import { name as appName } from './app.json';
 import App from './src';
 import './src/Functions/OpenAppTimes';
 
-// Register background handler
 messaging().setBackgroundMessageHandler(async remoteMessage => {
 	handleSetNotification();
 	console.log('Message handled in the background!', remoteMessage);
-});
-
-messaging().onNotificationOpenedApp(async remoteMessage => {
-	if (remoteMessage.data.deeplinking) {
-		await Linking.openURL(remoteMessage.data.deeplinking);
-	} else if (remoteMessage.data.url) {
-		await Linking.openURL(remoteMessage.data.url);
-	}
-});
-
-messaging().onMessage(async remoteMessage => {
-	async function onOpen() {
-		if (remoteMessage.data.deeplinking) {
-			await Linking.openURL(remoteMessage.data.deeplinking);
-		} else if (remoteMessage.data.url) {
-			await Linking.openURL(remoteMessage.data.url);
-		}
-	}
-
-	showMessage({
-		message: remoteMessage.notification.title,
-		description: remoteMessage.notification.body,
-		onPress: onOpen,
-	});
 });
 
 AppRegistry.registerComponent(appName, () => Sentry.wrap(App));
