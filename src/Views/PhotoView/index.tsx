276aa1d36bf89a6684a64d677a59d4a5410bf8b0
Douglas <7841509+douglasndm@users.noreply.github.com>
2023-02-02 11:16:38 -0300

    Moving photo view to shared components

--- src/Views/PhotoView/index.tsx
+++ src/Views/PhotoView/index.tsx
@@ -1,15 +1,11 @@
-import React, { useCallback, useEffect, useState } from 'react';
-import { useRoute, useNavigation } from '@react-navigation/native';
-import { StackNavigationProp } from '@react-navigation/stack';
-import { exists } from 'react-native-fs';
+import React, { useState, useCallback, useEffect } from 'react';
+import { useRoute } from '@react-navigation/native';
+
+import { getProductImagePath } from '@expirychecker/Functions/Products/Image';
 
 import Loading from '@components/Loading';
-import BackButton from '@components/BackButton';
-import Button from '@components/Button';
-import { getProductImagePath } from '~/Functions/Products/Image';
-import strings from '../../Locales';
 
-import { Container, PageHeader, PageTitle, Image } from './styles';
+import PhotoViewer from '@views/Product/PhotoView';
 
 interface Props {
 	productId: number;
@@ -22,47 +18,24 @@ const PhotoView: React.FC<Props> = () => {
 	const { params } = useRoute();
 	const routeParams = params as Props;
 
-	const { goBack } = useNavigation<StackNavigationProp<RoutesParams>>();
-
 	const loadData = useCallback(async () => {
-		setIsLoading(true);
-		const imagePath = await getProductImagePath(routeParams.productId);
+		try {
+			setIsLoading(true);
+			const imagePath = await getProductImagePath(routeParams.productId);
 
-		if (imagePath) {
-			const fileExists = await exists(imagePath);
-			if (fileExists) {
-				setPhotoPath(`file://${imagePath}`);
+			if (imagePath) {
+				setPhotoPath(imagePath);
 			}
+		} finally {
+			setIsLoading(false);
 		}
-		setIsLoading(false);
 	}, [routeParams.productId]);
 
 	useEffect(() => {
 		loadData();
-	}, [loadData]);
+	}, []);
 
-	return (
-		<>
-			{isLoading ? (
-				<Loading />
-			) : (
-				<Container>
-					<PageHeader>
-						<BackButton handleOnPress={goBack} />
-						<PageTitle>
-							{strings.View_PhotoView_PageTitle}
-						</PageTitle>
-					</PageHeader>
-
-					{!!photoPath && <Image source={{ uri: photoPath }} />}
-					<Button
-						text={strings.View_PhotoView_Button_Back}
-						onPress={goBack}
-					/>
-				</Container>
-			)}
-		</>
-	);
+	return isLoading ? <Loading /> : <PhotoViewer path={photoPath} />;
 };
 
 export default PhotoView;
