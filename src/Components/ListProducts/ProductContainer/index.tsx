21fec0ad62a067741b3c7a9bdedc7b6f9ebd5d15
Douglas <7841509+douglasndm@users.noreply.github.com>
2023-03-19 16:40:18 -0300

    fix: on change of user to PRO fast sub was not hidden

--- src/Components/ListProducts/ProductContainer/index.tsx
+++ src/Components/ListProducts/ProductContainer/index.tsx
@@ -1,42 +1,42 @@
 import React, { useContext, useMemo } from 'react';
 
-import ProductCard from '~/Components/ListProducts/ProductCard';
+import PreferencesContext from '@expirychecker/Contexts/PreferencesContext';
 
-import PreferencesContext from '~/Contexts/PreferencesContext';
+import ProductCard from '@expirychecker/Components/ListProducts/ProductCard';
+import FastSubscription from '@expirychecker/Components/FastSubscription';
 
 import { Container } from './styles';
-import FastSubscription from '~/Components/FastSubscription';
 
 interface RequestProps {
-    product: IProduct;
-    index?: number;
-    disableAds?: boolean;
-    handleEnableSelect: () => void;
+	product: IProduct;
+	index?: number;
+	disableAds?: boolean;
+	handleEnableSelect: () => void;
 }
 
 const ProductContainer: React.FC<RequestProps> = ({
-    product,
-    index,
-    disableAds,
-    handleEnableSelect,
+	product,
+	index,
+	disableAds,
+	handleEnableSelect,
 }: RequestProps) => {
-    const { userPreferences } = useContext(PreferencesContext);
+	const { userPreferences } = useContext(PreferencesContext);
 
-    const showAd = useMemo(() => {
-        if (disableAds) return false;
-        if (userPreferences.disableAds) return false;
-        if (index === 0) return false;
-        if (index && index % 5 === 0) return true;
-        return false;
-    }, [disableAds, userPreferences.disableAds, index]);
+	const showAd = useMemo(() => {
+		if (disableAds) return false;
+		if (userPreferences.disableAds || userPreferences.isPRO) return false;
+		if (index === 0) return false;
+		if (index && index % 5 === 0) return true;
+		return false;
+	}, [disableAds, userPreferences.disableAds, userPreferences.isPRO, index]);
 
-    return (
-        <Container>
-            {showAd && <FastSubscription />}
+	return (
+		<Container>
+			{showAd && <FastSubscription />}
 
-            <ProductCard product={product} onLongPress={handleEnableSelect} />
-        </Container>
-    );
+			<ProductCard product={product} onLongPress={handleEnableSelect} />
+		</Container>
+	);
 };
 
 export default React.memo(ProductContainer);
