8b08065cfbf3fcbc4f33092fc57f48bbf542c305
Douglas <7841509+douglasndm@users.noreply.github.com>
2023-03-23 13:54:30 -0300

    Moving some categories functions to dedicate files

---
+++ src/Utils/Categories/Create.ts
@@ -0,0 +1,38 @@
+import { UpdateMode } from 'realm';
+import UUID from 'react-native-uuid-generator';
+
+import realm from '@expirychecker/Services/Realm';
+
+import strings from '@expirychecker/Locales';
+
+import { getAllCategories } from './All';
+
+// this return the category uuid
+async function createCategory(categoryName: string): Promise<ICategory> {
+	const categories = await getAllCategories();
+
+	const categoryAlreadyExists = categories.find(
+		category => category.name.toLowerCase() === categoryName.toLowerCase()
+	);
+
+	if (categoryAlreadyExists) {
+		throw new Error(
+			strings.Function_Category_AddCategory_Error_AlreadyExists
+		);
+	}
+
+	const categoryUuid = await UUID.getRandomUUID();
+
+	const category: ICategory = {
+		id: categoryUuid,
+		name: categoryName.trim(),
+	};
+
+	realm.write(() => {
+		realm.create<ICategory>('Category', category, UpdateMode.Never);
+	});
+
+	return category;
+}
+
+export { createCategory };
