259ef0c79d3a51116cda79b104d4738debefd672
Douglas <7841509+douglasndm@users.noreply.github.com>
2023-06-15 21:49:36 -0300

    Upgrading styled components

--- src/Components/DrawerMenu/Logo/styles.ts
+++ src/Components/DrawerMenu/Logo/styles.ts
@@ -3,18 +3,18 @@ import { Dimensions, Platform } from 'react-native';
 
 import LogoImg from '~/Assets/Logo.png';
 
-export const Container = styled.TouchableWithoutFeedback``;
+export const Container = styled.Pressable``;
 
 export const LogoImage = styled.Image.attrs(() => ({
-    source: LogoImg,
-    resizeMode: 'cover',
+	source: LogoImg,
+	resizeMode: 'cover',
 }))`
-    width: 120px;
-    height: 120px;
+	width: 120px;
+	height: 120px;
 
-    ${Dimensions.get('window').height > 920 &&
-    Platform.OS === 'ios' &&
-    css`
-        margin-top: 50px;
-    `}
+	${Dimensions.get('window').height > 920 &&
+	Platform.OS === 'ios' &&
+	css`
+		margin-top: 50px;
+	`}
 `;
