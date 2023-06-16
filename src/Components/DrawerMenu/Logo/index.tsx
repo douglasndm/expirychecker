85e4006539fb37d58f0a9fbd5b9087f2ec80b533
Douglas <7841509+douglasndm@users.noreply.github.com>
2023-06-15 21:49:36 -0300

    Upgrading styled components

--- src/Components/DrawerMenu/Logo/index.tsx
+++ src/Components/DrawerMenu/Logo/index.tsx
@@ -3,11 +3,11 @@ import React from 'react';
 import { Container, LogoImage } from './styles';
 
 const Logo: React.FC = () => {
-    return (
-        <Container>
-            <LogoImage />
-        </Container>
-    );
+	return (
+		<Container>
+			<LogoImage />
+		</Container>
+	);
 };
 
 export default Logo;
