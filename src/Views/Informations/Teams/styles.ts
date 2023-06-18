a3b397ea47d8b4f364c1642a318287d4cc326dbb
Douglas <7841509+douglasndm@users.noreply.github.com>
2023-06-18 15:40:05 -0300

    Small ajusts on teams informations

--- src/Views/Informations/Teams/styles.ts
+++ src/Views/Informations/Teams/styles.ts
@@ -1,50 +1,40 @@
 import styled from 'styled-components/native';
-import { Button } from 'react-native-paper';
-import Ionicons from 'react-native-vector-icons/Ionicons';
 
-export const Container = styled.ScrollView`
-    background-color: ${({ theme }) => theme.colors.background};
-    flex: 1;
+export const Container = styled.SafeAreaView`
+	background-color: ${({ theme }) => theme.colors.background};
+	flex: 1;
 `;
 
-export const Content = styled.View`
-    margin: 15px 15px 0;
+export const Content = styled.ScrollView`
+	margin: 15px 15px 0;
 `;
 
 export const Title = styled.Text`
-    font-size: 22px;
-    font-weight: bold;
+	font-size: 22px;
+	font-weight: bold;
 
-    margin-bottom: 10px;
-    color: ${({ theme }) => theme.colors.text};
+	margin-bottom: 10px;
+	color: ${({ theme }) => theme.colors.text};
 `;
 
 export const Advantage = styled.Text`
-    font-size: 20px;
-    margin-bottom: 10px;
-    color: ${({ theme }) => theme.colors.text};
+	font-size: 20px;
+	margin-bottom: 10px;
+	color: ${({ theme }) => theme.colors.text};
 `;
 
 export const Text = styled.Text`
-    font-size: 16px;
-    margin-bottom: 7px;
-    font-family: 'Open Sans';
-    color: ${({ theme }) => theme.colors.text};
+	font-size: 16px;
+	margin-bottom: 7px;
+	font-family: 'Open Sans';
+	color: ${({ theme }) => theme.colors.text};
 `;
 
 export const WarningText = styled.Text`
-    font-size: 18px;
-    font-weight: bold;
-    margin-bottom: 7px;
-    font-family: 'Open Sans';
-    color: red;
-    text-align: center;
-`;
-
-export const ActionButton = styled(Button).attrs(props => ({
-    color: props.theme.colors.accent,
-}))``;
-
-export const Icons = styled(Ionicons)`
-    color: ${({ theme }) => theme.colors.text};
+	font-size: 18px;
+	font-weight: bold;
+	margin-bottom: 7px;
+	font-family: 'Open Sans';
+	color: red;
+	text-align: center;
 `;
