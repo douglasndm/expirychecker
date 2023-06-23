063d35171e280ef962ff654662c1c9cac14406a1
Douglas <7841509+douglasndm@users.noreply.github.com>
2023-06-23 20:52:12 -0300

    fix: onbarcoderead

--- src/Components/Product/Inputs/Pickers/Brand/index.tsx
+++ src/Components/Product/Inputs/Pickers/Brand/index.tsx
@@ -1,107 +1,107 @@
 import React, { useState, useEffect, useCallback, useContext } from 'react';
 import { ViewStyle } from 'react-native';
 
-import strings from '~/Locales';
+import strings from '@expirychecker/Locales';
 
-import PreferencesContext from '~/Contexts/PreferencesContext';
+import PreferencesContext from '@expirychecker/Contexts/PreferencesContext';
 
-import { createBrand, getAllBrands } from '~/Utils/Brands';
+import { createBrand, getAllBrands } from '@expirychecker/Utils/Brands';
 
 import { PickerContainer, Picker } from '../styles';
 
 interface Props {
-    onChange: (value: string) => void;
-    containerStyle?: ViewStyle;
-    defaultValue?: string | null;
+	onChange: (value: string) => void;
+	containerStyle?: ViewStyle;
+	defaultValue?: string | null;
 }
 
 export interface IBrandPickerRef {
-    onChange: (value: string) => void;
-    containerStyle?: ViewStyle;
-    defaultValue?: string | null;
+	onChange: (value: string) => void;
+	containerStyle?: ViewStyle;
+	defaultValue?: string | null;
 }
 
 const Brand = React.forwardRef<IBrandPickerRef>((props, ref) => {
-    const { defaultValue, containerStyle, onChange } = props;
+	const { defaultValue, containerStyle, onChange } = props;
 
-    const { userPreferences } = useContext(PreferencesContext);
+	const { userPreferences } = useContext(PreferencesContext);
 
-    const [selectedBrand, setSelectedBrand] = useState<string | null>(() => {
-        if (defaultValue && defaultValue !== '') {
-            return defaultValue;
-        }
-        return null;
-    });
+	const [selectedBrand, setSelectedBrand] = useState<string | null>(() => {
+		if (defaultValue && defaultValue !== '') {
+			return defaultValue;
+		}
+		return null;
+	});
 
-    const [brands, setBrands] = useState<Array<IBrandItem>>([]);
+	const [brands, setBrands] = useState<Array<IBrandItem>>([]);
 
-    const handleOnChange = useCallback(
-        value => {
-            setSelectedBrand(value);
+	const handleOnChange = useCallback(
+		value => {
+			setSelectedBrand(value);
 
-            // call on change on parent to update value their
-            onChange(value);
-        },
-        [onChange]
-    );
+			// call on change on parent to update value their
+			onChange(value);
+		},
+		[onChange]
+	);
 
-    const loadData = useCallback(async () => {
-        const allBrands = await getAllBrands();
-        const brandsArray: Array<IBrandItem> = [];
+	const loadData = useCallback(async () => {
+		const allBrands = await getAllBrands();
+		const brandsArray: Array<IBrandItem> = [];
 
-        allBrands.forEach(brand =>
-            brandsArray.push({
-                key: brand.id,
-                label: brand.name,
-                value: brand.id,
-            })
-        );
-        setBrands(brandsArray);
-    }, []);
+		allBrands.forEach(brand =>
+			brandsArray.push({
+				key: brand.id,
+				label: brand.name,
+				value: brand.id,
+			})
+		);
+		setBrands(brandsArray);
+	}, []);
 
-    useEffect(() => {
-        loadData();
-    }, []);
+	useEffect(() => {
+		loadData();
+	}, []);
 
-    const selectByName = useCallback(
-        async (name: string) => {
-            const brand = brands.find(
-                b => b.label.trim().toLowerCase() === name.trim().toLowerCase()
-            );
+	const selectByName = useCallback(
+		async (name: string) => {
+			const brand = brands.find(
+				b => b.label.trim().toLowerCase() === name.trim().toLowerCase()
+			);
 
-            if (!brand) {
-                const createdBrand = await createBrand(name);
+			if (!brand) {
+				const createdBrand = await createBrand(name);
 
-                await loadData();
+				await loadData();
 
-                setSelectedBrand(createdBrand.id);
-            } else if (brand) {
-                setSelectedBrand(brand.key);
-            }
-        },
-        [brands, loadData]
-    );
+				setSelectedBrand(createdBrand.id);
+			} else if (brand) {
+				setSelectedBrand(brand.key);
+			}
+		},
+		[brands, loadData]
+	);
 
-    React.useImperativeHandle(ref, () => ({
-        onChange,
-        setSelectedBrand,
-        selectByName,
-    }));
+	React.useImperativeHandle(ref, () => ({
+		onChange,
+		setSelectedBrand,
+		selectByName,
+	}));
 
-    return (
-        <PickerContainer style={containerStyle}>
-            <Picker
-                items={brands}
-                onValueChange={handleOnChange}
-                value={selectedBrand}
-                placeholder={{
-                    color: userPreferences.appTheme.colors.placeholderColor,
-                    label: strings.View_AddProduct_InputPlaceholder_SelectBrand,
-                    value: 'null',
-                }}
-            />
-        </PickerContainer>
-    );
+	return (
+		<PickerContainer style={containerStyle}>
+			<Picker
+				items={brands}
+				onValueChange={handleOnChange}
+				value={selectedBrand}
+				placeholder={{
+					color: userPreferences.appTheme.colors.placeholderColor,
+					label: strings.View_AddProduct_InputPlaceholder_SelectBrand,
+					value: 'null',
+				}}
+			/>
+		</PickerContainer>
+	);
 });
 
 export default Brand;
