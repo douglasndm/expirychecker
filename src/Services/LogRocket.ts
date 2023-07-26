9897f831cc344b048f218d1688ab592580283e0f
Douglas <7841509+douglasndm@users.noreply.github.com>
2023-07-26 11:32:48 -0300

    just small settings

--- src/Services/LogRocket.ts
+++ src/Services/LogRocket.ts
@@ -1,7 +1,7 @@
 import LogRocket from '@logrocket/react-native';
 import Config from 'react-native-config';
 
-const enable = true;
+const enable = false;
 
 if (!__DEV__ && enable) {
 	LogRocket.init(Config.LOGROCKET_APP_ID || '');
