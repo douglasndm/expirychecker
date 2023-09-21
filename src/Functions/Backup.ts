91e85ec60a7b78e1fe4236e24b0c37ce37c77298
Douglas <7841509+douglasndm@users.noreply.github.com>
2023-09-21 04:41:53 -0300

    Remake of backup function

--- src/Functions/Backup.ts
+++ src/Functions/Backup.ts
@@ -5,150 +5,19 @@ import RNFS, {
 	readDir,
 	unlink,
 } from 'react-native-fs';
-import { unzip, zip } from 'react-native-zip-archive';
+import { unzip } from 'react-native-zip-archive';
 import DocumentPicker from 'react-native-document-picker';
-import Share from 'react-native-share';
 import CryptoJS from 'crypto-js';
 import EnvConfig from 'react-native-config';
 
 import strings from '@expirychecker/Locales';
 
-import { getAllBrands, saveManyBrands } from '@expirychecker/Utils/Brands';
+import { saveManyBrands } from '@expirychecker/Utils/Brands';
 import { saveManyCategories } from '@expirychecker/Utils/Categories/SaveMany';
-import { getAllCategories } from '@expirychecker/Utils/Categories/All';
 
-import { getAllProducts, saveMany } from './Products';
+import { saveMany } from './Products';
 
 const backupDir = `${DocumentDirectoryPath}/backupDir`;
-
-async function genereteZipImagesFolder(): Promise<string> {
-	const allProducts = await getAllProducts({});
-
-	const productsWithPics = allProducts.filter(p => p.photo);
-	const dir = await readDir(DocumentDirectoryPath);
-
-	const files: Array<IProductImage> = [];
-
-	productsWithPics.forEach(p => {
-		const findedPic = dir.find(
-			file => file.name === p.photo || file.path === p.photo
-		);
-
-		if (findedPic) {
-			files.push({
-				productId: p.id,
-				imageName: findedPic.name,
-				imagePath: findedPic.path,
-			});
-		}
-	});
-
-	const targetPath = `${backupDir}/backupImages.zip`;
-	const sourcePath = `${DocumentDirectoryPath}/images`;
-
-	if (await exists(targetPath)) {
-		await unlink(targetPath);
-	}
-
-	// this will force the creation of images folder if it has been created, to avoid more verifications
-	if (!(await exists(sourcePath))) {
-		await mkdir(sourcePath);
-	}
-
-	const zipPath = await zip(sourcePath, targetPath);
-
-	return zipPath;
-}
-
-export async function generateBackupFile({
-	store,
-}: generateBackupFileProps): Promise<string> {
-	if (!(await exists(`${backupDir}`))) {
-		await mkdir(`${backupDir}`);
-	} else {
-		await unlink(`${backupDir}`);
-		await mkdir(`${backupDir}`);
-	}
-
-	const allProducts = await getAllProducts({});
-	const brands = await getAllBrands();
-	const categories = await getAllCategories();
-
-	let result;
-
-	result = {
-		categories,
-		brands,
-	};
-
-	if (store && store !== 'none') {
-		const filtedProducts = allProducts.filter(prod => {
-			if (prod.store?.includes(store)) {
-				return true;
-			}
-
-			return false;
-		});
-
-		result = {
-			...result,
-			products: filtedProducts,
-		};
-	} else if (store === 'none') {
-		const filtedProducts = allProducts.filter(prod => {
-			if (!prod.store) {
-				return true;
-			}
-
-			return false;
-		});
-
-		result = {
-			...result,
-			products: filtedProducts,
-		};
-	} else {
-		result = {
-			...result,
-			products: allProducts,
-		};
-	}
-
-	const encryptedProducts = CryptoJS.AES.encrypt(
-		JSON.stringify(result),
-		EnvConfig.APPLICATION_SECRET_BACKUP_CRYPT || ''
-	).toString();
-
-	await genereteZipImagesFolder();
-
-	const productsFilePath = `${backupDir}/${strings.Function_Export_FileName}.cvbf`;
-
-	// VERIFICA SE O ARQUIVO EXISTE E CASO EXISTA APAGUE ELE
-	// POR ALGUM MOTIVO A LIB FAZ APPEND AUTOMATICO
-	if (await RNFS.exists(productsFilePath)) {
-		await RNFS.unlink(productsFilePath);
-	}
-
-	await RNFS.writeFile(productsFilePath, encryptedProducts, 'utf8');
-
-	return productsFilePath;
-}
-
-export async function exportBackupFile(): Promise<void> {
-	await generateBackupFile({});
-
-	const zipPath = await zip(
-		`${backupDir}`,
-		`${DocumentDirectoryPath}/${strings.Function_Export_FileName}.zip`
-	);
-
-	await Share.open({
-		title: strings.Function_Share_SaveFileTitle,
-		url: `file://${zipPath}`,
-	});
-
-	await unlink(`${backupDir}`);
-}
 
 export async function importBackupFile(): Promise<void> {
 	const filePicked = await DocumentPicker.pickSingle({
