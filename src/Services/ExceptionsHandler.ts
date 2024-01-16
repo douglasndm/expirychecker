96f64839eefcf0c12ba5a26450027edc712a8a97
Douglas <7841509+douglasndm@users.noreply.github.com>
2024-01-16 03:43:33 -0300

    Adding crashlytics back grr

--- src/Services/ExceptionsHandler.ts
+++ src/Services/ExceptionsHandler.ts
@@ -1,7 +1,10 @@
+import Crashlytics from '@react-native-firebase/crashlytics';
+
 function captureException(error: Error) {
 	console.error(error);
 
 	if (!__DEV__) {
+		Crashlytics().recordError(error);
 	}
 }
 
