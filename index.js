added6a290c9caeac5040c87fc86f68267988dad
Douglas <7841509+douglasndm@users.noreply.github.com>
2024-01-18 04:36:25 -0300

    Adding Bugsnag navigator

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-import Bugsnag from '@bugsnag/react-native';
 import { AppRegistry } from 'react-native';
 
+import '@expirychecker/Services/Bugsnag';
 import '@expirychecker/Services/BackgroundNotifications';
 import { requestNotificationPermission } from '@services/Notifications/Permission';
 import '@services/Notifications';
@@ -9,8 +9,6 @@ import '@services/PushNotificationHandler';
 import { name as appName } from './app.json';
 import App from './src';
 import './src/Functions/OpenAppTimes';
-
-Bugsnag.start();
 
 requestNotificationPermission();
 
