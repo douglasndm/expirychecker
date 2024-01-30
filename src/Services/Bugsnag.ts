f02dab8b2251ccd981b67ed875cbf5051478b195
Douglas <7841509+douglasndm@users.noreply.github.com>
2024-01-30 20:00:05 -0300

    Nothing changed, just changes from anothes packages

--- src/Services/Bugsnag.ts
+++ src/Services/Bugsnag.ts
@@ -1,8 +1,6 @@
 import Bugsnag from '@bugsnag/react-native';
 import BugsnagPluginReactNavigation from '@bugsnag/plugin-react-navigation';
 
-import packageInfo from '@expirychecker/../package.json';
-
 Bugsnag.start({
 	plugins: [new BugsnagPluginReactNavigation()],
 	onError: () => {
