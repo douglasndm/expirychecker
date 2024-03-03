896c48babd57d4472ed34fff9fefd6030ff85985
Douglas <7841509+douglasndm@users.noreply.github.com>
2024-03-03 18:11:56 -0300

    adsConsent will not load if internet is not reachable

--- src/Components/Ads/AdsConsent/index.tsx
+++ src/Components/Ads/AdsConsent/index.tsx
@@ -1,12 +1,17 @@
 import React, { useEffect, useCallback } from 'react';
 import { AdsConsent } from 'react-native-google-mobile-ads';
+import { useNetInfo } from '@react-native-community/netinfo';
 
 import { captureException } from '@services/ExceptionsHandler';
 
 import { startGoogleMobileAdsSDK } from '@expirychecker/Services/Admob';
 
 const AdsConsentComponent: React.FC = () => {
+	const { isInternetReachable } = useNetInfo();
+
 	const loadData = useCallback(async () => {
+		if (!isInternetReachable) return;
+
 		try {
 			const adsConsentUpdateInfo = await AdsConsent.requestInfoUpdate();
 
@@ -23,7 +28,7 @@ const AdsConsentComponent: React.FC = () => {
 				captureException(error);
 			}
 		}
-	}, []);
+	}, [isInternetReachable]);
 
 	useEffect(() => {
 		loadData();
