5321ea1f49dc11effbdce48a17f7377942c2e90a
Douglas <7841509+douglasndm@users.noreply.github.com>
2024-03-12 21:11:56 -0300

    fix: open paywall from notification data

--- src/Services/BackgroundNotifications.ts
+++ src/Services/BackgroundNotifications.ts
@@ -1,7 +1,14 @@
 import messaging from '@react-native-firebase/messaging';
+
 import { handleSetNotification } from '@expirychecker/Services/BackgroundJobs';
+
+import { handlePurchase } from '@utils/Purchases/HandlePurchase';
 
 messaging().setBackgroundMessageHandler(async remoteMessage => {
 	handleSetNotification();
 	console.log('Message handled in the background!', remoteMessage);
+
+	const { paywall } = remoteMessage.data;
+
+	await handlePurchase(String(paywall));
 });
