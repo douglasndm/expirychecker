f267838585c9b217911f89b0b872cf71e838a404
Douglas <7841509+douglasndm@users.noreply.github.com>
2024-03-26 15:07:16 -0300

    Adding multiples disable firestore network to avoid non-pros to use resources

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 import { AppRegistry } from 'react-native';
+import firestore from '@react-native-firebase/firestore';
 
 import '@services/Bugsnag';
 import '@expirychecker/Services/BackgroundNotifications';
@@ -8,5 +9,7 @@ import '@services/PushNotificationHandler';
 import { name as appName } from './app.json';
 import App from './src';
 import './src/Functions/OpenAppTimes';
+
+firestore().disableNetwork();
 
 AppRegistry.registerComponent(appName, () => App);
