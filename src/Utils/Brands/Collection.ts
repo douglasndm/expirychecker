c8bab581dce0903fabb80dcd24a3dbf7c526a924
Douglas <7841509+douglasndm@users.noreply.github.com>
2024-04-05 21:00:46 -0300

    fix: initial check

--- src/Utils/Brands/Collection.ts
+++ src/Utils/Brands/Collection.ts
@@ -2,17 +2,11 @@ import Auth from '@react-native-firebase/auth';
 import Firestore, {
 	FirebaseFirestoreTypes,
 } from '@react-native-firebase/firestore';
-import AsyncStorage from '@react-native-async-storage/async-storage';
 
 async function getCollectionPath(): Promise<
 	| FirebaseFirestoreTypes.CollectionReference<FirebaseFirestoreTypes.DocumentData>
 	| undefined
 > {
-	const initialSyncDone = await AsyncStorage.getItem('initialSync');
-	if (!initialSyncDone) {
-		// console.log('Initial sync not done, not syncing Realm with Firestore');
-		return undefined;
-	}
 	const user = Auth().currentUser;
 
 	if (user && user.email) {
