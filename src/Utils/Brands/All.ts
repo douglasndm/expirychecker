efde617f0b60ee277f53bfdd8c4c9bafa728b5bc
Douglas <7841509+douglasndm@users.noreply.github.com>
2024-04-06 00:47:53 -0300

    Adding update on realm changes on firestore brand update

--- src/Utils/Brands/All.ts
+++ src/Utils/Brands/All.ts
@@ -1,3 +1,5 @@
+import { UpdateMode } from 'realm';
+
 import realm from '@expirychecker/Services/Realm';
 
 import { getCollectionPath } from './Collection';
@@ -16,6 +18,33 @@ async function getAllBrands(): Promise<IBrand[]> {
 	}
 
 	const brandsQuerySnapshot = await brandsCollection.get();
+
+	brandsCollection.onSnapshot(snapshot => {
+		if (!snapshot) {
+			return;
+		}
+
+		const changes = snapshot.docChanges();
+
+		changes.forEach(change => {
+			const brand = change.doc.data();
+
+			const updatedBrand = {
+				id: brand.id,
+				name: brand.name,
+				created_at: brand.created_at
+					? brand.created_at.toDate()
+					: undefined,
+				updated_at: brand?.updated_at
+					? brand.updated_at.toDate()
+					: undefined,
+			};
+
+			realm.write(() => {
+				realm.create('Brand', updatedBrand, UpdateMode.Modified);
+			});
+		});
+	});
 
 	brandsQuerySnapshot.forEach(documentSnapshot => {
 		brands.push(documentSnapshot.data() as IBrand);
