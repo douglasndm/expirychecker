f9c72fee5f8722c110236458e584be786dd3e742
Douglas <7841509+douglasndm@users.noreply.github.com>
2024-08-29 01:50:13 -0300

    Adding Sentry init config

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
-import { AppRegistry, View, Text } from 'react-native';
+import { AppRegistry } from 'react-native';
+import EnvConfig from 'react-native-config';
+import * as Sentry from "@sentry/react-native";
 
 import '@expirychecker/Services/BackgroundNotifications';
 import '@services/PushNotificationHandler';
@@ -7,4 +9,17 @@ import { name as appName } from './app.json';
 import App from './src';
 import './src/Functions/OpenAppTimes';
 
-AppRegistry.registerComponent(appName, () => App);
+
+Sentry.init({
+  dsn: EnvConfig.SENTRY_DSN,
+  // Set tracesSampleRate to 1.0 to capture 100% of transactions for performance monitoring.
+  // We recommend adjusting this value in production.
+  tracesSampleRate: 1.0,
+  _experiments: {
+    // profilesSampleRate is relative to tracesSampleRate.
+    // Here, we'll capture profiles for 100% of transactions.
+    profilesSampleRate: 1.0,
+  },
+});
+
+AppRegistry.registerComponent(appName, () => Sentry.wrap(App));
