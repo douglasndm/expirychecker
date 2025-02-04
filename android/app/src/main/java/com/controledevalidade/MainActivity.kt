ca05834b569ae3612ab1f1e425ed200658703f4e
Douglas <7841509+douglasndm@users.noreply.github.com>
2025-02-04 14:00:51 -0300

    fix: bootsplash load on android

--- android/app/src/main/java/com/controledevalidade/MainActivity.kt
+++ android/app/src/main/java/com/controledevalidade/MainActivity.kt
@@ -6,6 +6,8 @@ import com.facebook.react.ReactActivityDelegate
 import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint.fabricEnabled
 import com.facebook.react.defaults.DefaultReactActivityDelegate
 
+import com.zoontek.rnbootsplash.RNBootSplash
+
 class MainActivity : ReactActivity() {
 
    /**
@@ -22,6 +24,7 @@ class MainActivity : ReactActivity() {
         DefaultReactActivityDelegate(this, mainComponentName, fabricEnabled)
 
     override fun onCreate(savedInstanceState: Bundle?): Unit {
-        super.onCreate(null)
+        RNBootSplash.init(this, R.style.BootTheme) // ⬅️ initialize the splash screen
+        super.onCreate(null) // super.onCreate(null) with react-native-screens
     }
 }
